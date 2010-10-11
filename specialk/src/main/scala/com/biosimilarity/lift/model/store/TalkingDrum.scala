be5d3db1d583b3324a2dfb001dced8ee3727e871
leithaus <lgreg.meredith@gmail.com>
2010-10-11 01:00:01 -0700

    Intermediate drop to add distribution to TermStore

---
+++ specialk/src/main/scala/com/biosimilarity/lift/model/store/TalkingDrum.scala
@@ -0,0 +1,99 @@
+// -*- mode: Scala;-*- 
+// Filename:    SpecialKMessenger.scala 
+// Authors:     lgm                                                    
+// Creation:    Wed Aug 25 13:58:10 2010 
+// Copyright:   Not supplied 
+// Description: 
+// ------------------------------------------------------------------------
+
+package com.biosimilarity.lift.model.store
+
+import com.biosimilarity.lift.model.agent._
+import com.biosimilarity.lift.model.msg._
+import com.biosimilarity.lift.lib._
+
+import net.liftweb.amqp._
+
+import scala.collection.mutable._
+
+import java.net.URI
+import java.util.UUID
+
+trait DrumTalk {
+  type JSONOverAMQP = AMQPMessage[String]
+  type JTSReq = JustifiedRequest[JSONOverAMQP,JSONOverAMQP]
+  type JTSRsp = JustifiedResponse[JSONOverAMQP,JSONOverAMQP]
+
+  object IdVendor extends UUIDOps {
+    def getURI() = {
+      new URI( "com", "biosimilarity", getUUID().toString )
+    }
+  }
+
+  object AnAMQPTraceMonitor extends TraceMonitor[JSONOverAMQP,JSONOverAMQP]
+
+  def source(req : JSONOverAMQP, resp : JSONOverAMQP)
+  : URI = {
+    // temporarily
+    IdVendor.getURI()
+  }
+  def target( req : JSONOverAMQP, resp : JSONOverAMQP )
+  : URI = {
+    // temporarily
+    IdVendor.getURI()
+  }
+  def label( req : JSONOverAMQP, resp : JSONOverAMQP )
+  : UUID = {
+    // temporarily
+    IdVendor.getUUID()
+  }
+
+  class TalkingDrum(
+    alias : URI
+  )
+  extends ReflectiveMessenger[JSONOverAMQP,JSONOverAMQP](
+    alias,
+    new ListBuffer[JTSReq](),
+    new ListBuffer[JTSRsp](),
+    Some( new LinkedHashMap[URI,Socialite[JSONOverAMQP,JSONOverAMQP]]),
+    AnAMQPTraceMonitor
+  )
+  with UUIDOps {
+    override def validateTarget( msg : {def to : URI} ) : Boolean = {
+      // Put URI filtering behavior here
+      true
+    }
+    
+    override def validateAcquaintance( msg : {def from : URI} ) : Boolean = {
+      // Put Requestor filtering behavior here
+      nameSpace match {
+	case None => false
+	case Some( map ) => true
+      }
+    }
+
+    override def handleWithContinuation(
+      request : JTSReq,
+      k : Status[JTSReq] => Status[JTSReq]
+    ) = {
+      //println( "handling: " + request )
+      request match {
+	case JustifiedRequest(
+	  msgId, trgt, src, lbl, body, None
+	) => { 
+	  // Handle a justified request with no initiating response
+	}
+	case _ => {
+	  // Handle a justified request with an initiating response
+	}
+      }
+      JReqStatus(
+	request,
+	true,
+	None,
+	Some( k )
+      )
+    }  
+  }
+  
+}
--- specialk/src/main/scala/com/biosimilarity/lift/model/store/Tribe.scala
+++ specialk/src/main/scala/com/biosimilarity/lift/model/store/Tribe.scala
@@ -9,19 +9,144 @@
 package com.biosimilarity.lift.model.store
 
 import com.biosimilarity.lift.lib._
+import net.liftweb.amqp._
 
 import scala.util.continuations._ 
+import scala.collection.Map
 import scala.collection.mutable.HashMap
+import scala.actors.Actor
+import scala.actors.Actor._
+
+import com.rabbitmq.client._
 
 import org.prolog4j._
 
-import java.net.URI
+import com.thoughtworks.xstream.XStream
+import com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver
 
-import net.liftweb.amqp._
+import java.util.UUID
+import java.net.URI
+import java.io.ByteArrayOutputStream
+import java.io.ObjectOutputStream
+
+trait Rabitter {
+  def configure(
+    cf : ConnectionFactory,
+    host : String,
+    port : Int
+  )(
+    channel: Channel
+  ) : Int
+
+  def rabbitFactory() : (ConnectionParameters, ConnectionFactory) = {
+    val params = new ConnectionParameters
+    // All of the params, exchanges, and queues are all just example data.
+    params.setUsername("guest")
+    params.setPassword("guest")
+    params.setVirtualHost("/")
+    params.setRequestedHeartbeat(0)
+    (params, new ConnectionFactory(params))
+  }
+
+  def rabbitTube(
+    cnxnFctry : ConnectionFactory,
+    host : String,
+    port : Int
+  ) = {
+    val conn = cnxnFctry.newConnection( host, port )
+    val channel = conn.createChannel()
+    val ticket = configure( cnxnFctry, host, port )( channel )
+
+    ( conn, channel, ticket )
+  }
+
+  def send [T] (
+    channel : Channel,
+    exchange : String,
+    routingKey : String,
+    ticket : Int
+    )(
+    msg: T
+  ) {
+      // Now write an object to a byte array and shove it across the wire.
+      val bytes = new ByteArrayOutputStream
+      val store = new ObjectOutputStream(bytes)
+      store.writeObject(msg)
+      store.close
+      channel.basicPublish(
+	ticket,
+	exchange,
+	routingKey,
+	null,
+	bytes.toByteArray
+      )
+    }  
+}
+
+trait OZ[GetRequest,GetContinuation] {
+  def hostToOutstandingReqs : Map[URI,(GetRequest,GetContinuation)]
+}
+
+trait Warren {
+  def selfIdentity : UUID
+  def jsonSender : Seq[StdJSONOverAMQPSender]
+  def jsonDispatcher : Seq[JSONAMQPListener]
+  def acquaintances : Seq[URI]
+
+  def tunnel() : ( Seq[StdJSONOverAMQPSender], Seq[JSONAMQPListener] ) = {
+    val sndrRcvrPairs =
+      for( acquaintance <- acquaintances )
+      yield {
+	(
+	  new StdJSONOverAMQPSender( acquaintance.getHost ), 
+	  new JSONAMQPListener( acquaintance.getHost )
+	)
+      }
+    sndrRcvrPairs.unzip
+  }
+}
 
 class Tribe[Namespace,Var,Tag,Value](
-  val jsonSender : StdJSONOverAMQPSender,
-  val jsonDispatcher : JSONAMQPListener,
-  val acquaintances : Seq[String]
-) extends TermStore[Namespace,Var,Tag,Value] {
+  override val selfIdentity : UUID,
+  override val jsonSender : Seq[StdJSONOverAMQPSender],
+  override val jsonDispatcher : Seq[JSONAMQPListener],
+  override val acquaintances : Seq[URI]
+) extends TermStore[Namespace,Var,Tag,Value]
+with Warren
+with DrumTalk {
+  
+  override def get(
+    path : CnxnCtxtLabel[Namespace,Var,Tag],
+    next : WhatNext
+  )
+  : Seq[Option[Resource]] = {        
+    for( placeNSoln <- places( path, Input ) )
+    yield {
+      val ( place, soln ) = placeNSoln
+      _labelMap.get( place ) match {
+	case sv @ Some( value ) => {
+	  _labelMap -= place
+	  sv
+	}
+	case None => {
+	  reset {
+	    val rslt : Option[Resource] = 
+	      shift {
+		( k : GetContinuation ) => {	      
+		  _waiters( place ) =
+		    _waiters.get( place ).getOrElse( Nil ) ++ List( k )
+		  
+		  k( None )
+		}	    	      
+	      }
+	    reportage( "resuming with value : " + rslt )
+	    rslt match {
+	      case Some( _ ) =>	next( rslt )
+	      case nv @ _ => nv
+	    }
+	  }
+	}	
+      }
+    }    
+  }
 }
