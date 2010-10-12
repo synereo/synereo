2fb3455de8c6ebaae2264fabb04cd7a1d5caefa1
leithaus <lgreg.meredith@gmail.com>
2010-10-12 01:58:07 -0700

    Another checkpoint toward Distributed TermStore

--- specialk/src/main/scala/com/biosimilarity/lift/model/store/AMQPTransport.scala
+++ specialk/src/main/scala/com/biosimilarity/lift/model/store/AMQPTransport.scala
@@ -15,14 +15,32 @@ import com.biosimilarity.lift.lib._
 import net.liftweb.amqp._
 
 import scala.collection.mutable._
+import scala.util.continuations._ 
+
+import scala.actors.Actor
+import scala.actors.Actor._
+
+import com.rabbitmq.client._
+
+import com.thoughtworks.xstream.XStream
+import com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver
 
 import java.net.URI
 import java.util.UUID
 
-trait AgentsOverAMQP {
-  type JSONOverAMQP = AMQPMessage[String]
-  type JTSReq = JustifiedRequest[JSONOverAMQP,JSONOverAMQP]
-  type JTSRsp = JustifiedResponse[JSONOverAMQP,JSONOverAMQP]
+trait DistributedTermSpaceMsg[Namespace,Var,Tag,Value]
+case class DGetRequest[Namespace,Var,Tag,Value](
+  path : CnxnCtxtLabel[Namespace,Var,Tag]
+) extends DistributedTermSpaceMsg[Namespace,Var,Tag,Value]
+case class DGetResponse[Namespace,Var,Tag,Value](
+  path : CnxnCtxtLabel[Namespace,Var,Tag],
+  value : Value
+) extends DistributedTermSpaceMsg[Namespace,Var,Tag,Value]
+
+trait AgentsOverAMQP[Namespace,Var,Tag,Value] {
+  type DMsg = DistributedTermSpaceMsg[Namespace,Var,Tag,Value]
+  type JTSReq = JustifiedRequest[DMsg,DMsg]
+  type JTSRsp = JustifiedResponse[DMsg,DMsg]
 
   object IdVendor extends UUIDOps {
     def getURI() = {
@@ -30,15 +48,15 @@ trait AgentsOverAMQP {
     }
   }
 
-  object AnAMQPTraceMonitor extends TraceMonitor[JSONOverAMQP,JSONOverAMQP]  
+  object AnAMQPTraceMonitor extends TraceMonitor[DMsg,DMsg]  
 
   class AMQPAgent(
     alias : URI
-  ) extends ReflectiveMessenger[JSONOverAMQP,JSONOverAMQP](
+  ) extends ReflectiveMessenger[DMsg,DMsg](
     alias,
     new ListBuffer[JTSReq](),
     new ListBuffer[JTSRsp](),
-    Some( new LinkedHashMap[URI,Socialite[JSONOverAMQP,JSONOverAMQP]]),
+    Some( new LinkedHashMap[URI,Socialite[DMsg,DMsg]]),
     AnAMQPTraceMonitor
   )
   with UUIDOps {
@@ -80,3 +98,145 @@ trait AgentsOverAMQP {
   }
   
 }
+
+class AgentTwistedPair[Namespace,Var,Tag,Value]( trgt : URI ) 
+extends AgentsOverAMQP[Namespace,Var,Tag,Value]
+with AbstractJSONAMQPListener {
+  
+  type JSONListener = AMQPAgent  
+
+  case object _jsonListener
+  extends AMQPAgent( trgt ) {
+    override def act () {
+      nameSpace match {
+	case None => {
+	  logError( name, this, NoNamespace() )
+	}
+	case Some( map ) => {
+	  receive {
+	    case msg@AMQPMessage( cntnt : String ) => {
+	      val h2o = rehydrate( cntnt ) 
+	      h2o match { 
+		case Left( jreq ) => this ! jreq 
+		case Right( jrsp ) => this ! jrsp
+	      }
+	    }
+	    case jr@JustifiedRequest(
+	      m, p, d, t,
+	      f : DMsg,
+	      c : Option[Response[AbstractJustifiedRequest[DMsg,DMsg],DMsg]]
+	    ) => {	    
+	      val jrJSON : JustifiedRequest[DMsg,DMsg]
+	      = jr.asInstanceOf[JustifiedRequest[DMsg,DMsg]]
+    
+	      if ( validate( jrJSON ) ) {
+		println( "calling handle on " + jr )
+		reset {
+		  shift {
+		    ( k : Status[JustifiedRequest[DMsg,DMsg]] => Status[JustifiedRequest[DMsg,DMsg]] )
+		  => {
+		    k( handleWithContinuation( jrJSON, k ) )
+		  }
+		  }
+		}
+	      }
+	      act()
+	    }
+	    case jr@JustifiedResponse(
+	      m, p, d, t,
+	      f : DMsg,
+	      c : Option[Request[AbstractJustifiedResponse[DMsg,DMsg],DMsg]]
+	    ) =>  {
+	      val jrJSON : JustifiedResponse[DMsg,DMsg]
+	      = jr.asInstanceOf[JustifiedResponse[DMsg,DMsg]]
+	      if ( validate( jrJSON ) ) {
+		println( "calling handle on " + jr )
+		reset {
+		  shift {
+		    ( k : Status[JustifiedResponse[DMsg,DMsg]] => Status[JustifiedResponse[DMsg,DMsg]] )
+		  => {
+		    k( handleWithContinuation( jrJSON, k ) )
+		  }
+		  }
+		}
+	      }
+	      act()
+	    }
+	    case ir@InspectRequests( t, f ) => {
+	      if ( validate( ir ) ) {
+		println( "calling handle on " + ir )
+		handle( ir )
+	      }
+	      act()
+	    }
+	    case ir@InspectResponses( t, f ) => {
+	      if ( validate( ir ) ) {
+		println( "calling handle on " + ir )
+		handle( ir )
+	      }
+	      act()
+	    }
+	    case ir@InspectNamespace( t, f ) => {
+	      if ( validate( ir ) ) {
+		println( "calling handle on " + ir )
+		handle( ir )
+	      }
+	      act()
+	    }
+	  }
+	}
+      }    
+    }
+  }
+
+  override def jsonListener() : JSONListener = {
+    _jsonListener
+  }  
+
+  override def host : String = { trgt.getHost }
+
+  case object _jsonSender
+  extends JSONAMQPSender(
+    rabbitFactory(),
+    host,
+    5672,
+    "mult",
+    "routeroute"
+  )
+  _jsonSender.start
+  
+  def jsonSender() : JSONAMQPSender = {
+    _jsonSender
+  }
+
+  def send( contents : java.lang.Object ) : Unit = {
+    _jsonSender ! AMQPMessage(
+      new XStream( new JettisonMappedXmlDriver() ).toXML( contents )
+    )
+  }  
+
+  def rehydrate( contents: String ) :
+  Either[
+    JustifiedRequest[DMsg,DMsg],
+    JustifiedResponse[DMsg,DMsg]
+  ] = {
+    val msg =
+      new XStream( new JettisonMappedXmlDriver() ).fromXML( contents )
+    msg match {
+      case jreq : JustifiedRequest[DMsg,DMsg] => {
+	Left[JustifiedRequest[DMsg,DMsg],JustifiedResponse[DMsg,DMsg]]( jreq )
+      }
+      case jrsp : JustifiedResponse[DMsg,DMsg] => {
+	Right[JustifiedRequest[DMsg,DMsg],JustifiedResponse[DMsg,DMsg]]( jrsp )
+      }
+      case _ => {
+	throw new Exception(
+	  "unexpected message type : " + msg.getClass
+	)
+      }
+    }
+  }
+  
+}
+
+
--- specialk/src/main/scala/com/biosimilarity/lift/model/store/Tribe.scala
+++ specialk/src/main/scala/com/biosimilarity/lift/model/store/Tribe.scala
@@ -35,137 +35,31 @@ trait OZ[GetRequest,GetContinuation] {
   def hostToOutstandingReqs : Map[URI,(GetRequest,GetContinuation)]
 }
 
-trait Collective {
+trait Collective[Namespace,Var,Tag,Value] {
   def selfIdentity : URI
-  def jsonSender : Seq[StdJSONOverAMQPSender]
-  def jsonDispatcher : Seq[JSONAMQPListener]
+  def agentTwistedPairs : Seq[AgentTwistedPair[Namespace,Var,Tag,Value]]
   def acquaintances : Seq[URI]
 
-  def tunnel() : ( Seq[StdJSONOverAMQPSender], Seq[JSONAMQPListener] ) = {
-    val sndrRcvrPairs =
-      for( acquaintance <- acquaintances )
-      yield {
-	(
-	  new StdJSONOverAMQPSender( acquaintance.getHost ), 
-	  new JSONAMQPListener( acquaintance.getHost )
-	)
-      }
-    sndrRcvrPairs.unzip
+  def tunnel() : Seq[AgentTwistedPair[Namespace,Var,Tag,Value]] = {
+    for( acquaintance <- acquaintances )
+    yield {
+      (
+	new AgentTwistedPair[Namespace,Var,Tag,Value]( acquaintance )
+      )
+    }
   }
 }
 
 class Tribe[Namespace,Var,Tag,Value](
   override val selfIdentity : URI,
-  override val jsonSender : Seq[StdJSONOverAMQPSender],
-  override val jsonDispatcher : Seq[JSONAMQPListener],
+  override val agentTwistedPairs
+  : Seq[AgentTwistedPair[Namespace,Var,Tag,Value]],
   override val acquaintances : Seq[URI]
 ) extends TermStore[Namespace,Var,Tag,Value]
-with Collective
-with AgentsOverAMQP
-with AbstractJSONAMQPListener {
-  
-  type JSONListener = AMQPAgent  
-
-  case object _jsonListener
-  extends AMQPAgent( selfIdentity ) {
-    override def act () {
-      nameSpace match {
-	case None => {
-	  logError( name, this, NoNamespace() )
-	}
-	case Some( map ) => {
-	  receive {
-	    case msg@AMQPMessage( cntnt : String ) => {
-	      val h2o = rehydrate( cntnt ) 
-	      h2o match { 
-		case Left( jreq ) => this ! jreq 
-		case Right( jrsp ) => this ! jrsp
-	      }
-	    }
-	    case jr@JustifiedRequest(
-	      m, p, d, t,
-	      f : JSONOverAMQP,
-	      c : Option[Response[AbstractJustifiedRequest[JSONOverAMQP,JSONOverAMQP],JSONOverAMQP]]
-	    ) => {	    
-	      val jrJSON : JustifiedRequest[JSONOverAMQP,JSONOverAMQP]
-	      = jr.asInstanceOf[JustifiedRequest[JSONOverAMQP,JSONOverAMQP]]
-    
-	      if ( validate( jrJSON ) ) {
-		println( "calling handle on " + jr )
-		reset {
-		  shift {
-		    ( k : Status[JustifiedRequest[JSONOverAMQP,JSONOverAMQP]] => Status[JustifiedRequest[JSONOverAMQP,JSONOverAMQP]] )
-		  => {
-		    k( handleWithContinuation( jrJSON, k ) )
-		  }
-		  }
-		}
-	      }
-	      act()
-	    }
-	    case jr@JustifiedResponse(
-	      m, p, d, t,
-	      f : JSONOverAMQP,
-	      c : Option[Request[AbstractJustifiedResponse[JSONOverAMQP,JSONOverAMQP],JSONOverAMQP]]
-	    ) =>  {
-	      val jrJSON : JustifiedResponse[JSONOverAMQP,JSONOverAMQP]
-	      = jr.asInstanceOf[JustifiedResponse[JSONOverAMQP,JSONOverAMQP]]
-	      if ( validate( jrJSON ) ) {
-		println( "calling handle on " + jr )
-		reset {
-		  shift {
-		    ( k : Status[JustifiedResponse[JSONOverAMQP,JSONOverAMQP]] => Status[JustifiedResponse[JSONOverAMQP,JSONOverAMQP]] )
-		  => {
-		    k( handleWithContinuation( jrJSON, k ) )
-		  }
-		  }
-		}
-	      }
-	      act()
-	    }
-	    case ir@InspectRequests( t, f ) => {
-	      if ( validate( ir ) ) {
-		println( "calling handle on " + ir )
-		handle( ir )
-	      }
-	      act()
-	    }
-	    case ir@InspectResponses( t, f ) => {
-	      if ( validate( ir ) ) {
-		println( "calling handle on " + ir )
-		handle( ir )
-	      }
-	      act()
-	    }
-	    case ir@InspectNamespace( t, f ) => {
-	      if ( validate( ir ) ) {
-		println( "calling handle on " + ir )
-		handle( ir )
-	      }
-	      act()
-	    }
-	  }
-	}
-      }    
-    }
-  }
-
-  override def jsonListener() : JSONListener = {
-    _jsonListener
-  }  
-
-  override def host : String = { selfIdentity.getHost }
-
-  trait DMsg
-  case class DGetRequest( path : CnxnCtxtLabel[Namespace,Var,Tag] )
-       extends DMsg
-  case class DGetResponse(
-    path : CnxnCtxtLabel[Namespace,Var,Tag],
-    value : Value
-  ) extends DMsg
+with Collective[Namespace,Var,Tag,Value] {  
 
   def forwardGet( path : CnxnCtxtLabel[Namespace,Var,Tag] ) : Unit = {
-    for( jsndr <- jsonSender ) {
+    for( jsndr <- agentTwistedPairs ) {
       jsndr.send( DGetRequest( path ) )
     }
   }
@@ -206,28 +100,6 @@ with AbstractJSONAMQPListener {
 	}	
       }
     }    
-  }
-
-  def rehydrate( contents: String ) :
-  Either[
-    JustifiedRequest[JSONOverAMQP,JSONOverAMQP],
-    JustifiedResponse[JSONOverAMQP,JSONOverAMQP]
-  ] = {
-    val msg =
-      new XStream( new JettisonMappedXmlDriver() ).fromXML( contents )
-    msg match {
-      case jreq : JustifiedRequest[JSONOverAMQP,JSONOverAMQP] => {
-	Left[JustifiedRequest[JSONOverAMQP,JSONOverAMQP],JustifiedResponse[JSONOverAMQP,JSONOverAMQP]]( jreq )
-      }
-      case jrsp : JustifiedResponse[JSONOverAMQP,JSONOverAMQP] => {
-	Right[JustifiedRequest[JSONOverAMQP,JSONOverAMQP],JustifiedResponse[JSONOverAMQP,JSONOverAMQP]]( jrsp )
-      }
-      case _ => {
-	throw new Exception(
-	  "unexpected message type : " + msg.getClass
-	)
-      }
-    }
-  }
+  }  
   
 }
