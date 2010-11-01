59f1091370177c9f34c495e709d8c1668a268a0b
leithaus <lgreg.meredith@gmail.com>
2010-11-01 12:45:25 -0700

    Another intermediate type honing in

--- specialk/src/main/scala/net/liftweb/amqp/JSONTwistedPair.scala
+++ specialk/src/main/scala/net/liftweb/amqp/JSONTwistedPair.scala
@@ -21,16 +21,26 @@ trait AMQPTwistedPair {
   def srcHost : String
   def trgtHost : String
 
-  def amqpSenderToSrc : StdJSONOverAMQPSender
-  def amqpReceiverFromTrgt : JSONAMQPListener
+  def amqpSenderToTrgt : StdJSONOverAMQPSender
+  def amqpReceiverFromSrc : JSONAMQPListener
 }
 
 class JSONAMQPTP(
   override val srcHost : String,
   override val trgtHost : String,
-  override val amqpSenderToSrc : StdJSONOverAMQPSender,
-  override val amqpReceiverFromTrgt : JSONAMQPListener
+  override val amqpSenderToTrgt : StdJSONOverAMQPSender,
+  override val amqpReceiverFromSrc : JSONAMQPListener
 ) extends AMQPTwistedPair 
+
+class JSONAMQPTwisted(
+  override val srcHost : String,
+  override val trgtHost : String
+) extends AMQPTwistedPair {
+  override lazy val amqpSenderToTrgt : StdJSONOverAMQPSender =
+    new StdJSONOverAMQPSender( trgtHost )
+  override lazy val amqpReceiverFromSrc : JSONAMQPListener =
+    new JSONAMQPListener( srcHost )
+}
 
 class JSONTwistedPair(
   srcHost : String, 
