df687ee8f0ed42af6b45ac3fbec49a428a773938
Meredith Gregory <lgreg.meredith@biosimilarity.com>
2011-06-28 17:06:25 -0700

    Moving logging scaffold to moniker branch

---
+++ specialk/src/main/scala/com/biosimilarity/lift/model/store/log/SelfMonitoring.scala
@@ -0,0 +1,79 @@
+// -*- mode: Scala;-*- 
+// Filename:    SelfMonitoring.scala 
+// Authors:     lgm                                                    
+// Creation:    Thu Jun 23 15:49:34 2011 
+// Copyright:   Not supplied 
+// Description: 
+// ------------------------------------------------------------------------
+
+package com.biosimilarity.lift.model.store.monitor
+
+import com.biosimilarity.lift.model.ApplicationDefaults
+
+import com.biosimilarity.lift.model.store.CnxnLabel
+import com.biosimilarity.lift.model.store.OntologicalStatus
+import com.biosimilarity.lift.model.store.Factual
+import com.biosimilarity.lift.model.store.Hypothetical
+import com.biosimilarity.lift.model.store.Theoretical
+import com.biosimilarity.lift.model.store.CnxnLeaf
+import com.biosimilarity.lift.model.store.CCnxnLeaf
+import com.biosimilarity.lift.model.store.AbstractCnxnBranch
+import com.biosimilarity.lift.model.store.CnxnBranch
+import com.biosimilarity.lift.model.store.CCnxnBranch
+import com.biosimilarity.lift.model.store.CnxnCtxtLabel
+import com.biosimilarity.lift.model.store.CnxnCtxtLeaf
+import com.biosimilarity.lift.model.store.CCnxnCtxtLeaf
+import com.biosimilarity.lift.model.store.AbstractCnxnCtxtBranch
+import com.biosimilarity.lift.model.store.CnxnCtxtBranch
+import com.biosimilarity.lift.model.store.CCnxnCtxtBranch
+import com.biosimilarity.lift.model.store.CnxnCtxtInjector
+import com.biosimilarity.lift.model.store.Cnxn
+import com.biosimilarity.lift.model.store.CCnxn
+import com.biosimilarity.lift.model.store.CnxnXML
+import com.biosimilarity.lift.model.store.Blobify
+import com.biosimilarity.lift.model.store.CnxnXQuery
+
+import com.biosimilarity.lift.model.store.xml._
+import com.biosimilarity.lift.model.agent._
+import com.biosimilarity.lift.model.msg._
+import com.biosimilarity.lift.lib._
+import com.biosimilarity.lift.lib.moniker._
+import com.biosimilarity.lift.lib.monad._
+
+import scala.concurrent.{Channel => Chan, _}
+import scala.concurrent.cpsops._
+import scala.util.continuations._ 
+import scala.xml._
+import scala.collection.MapProxy
+import scala.collection.mutable.Map
+import scala.collection.mutable.HashMap
+import scala.collection.mutable.LinkedHashMap
+import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.MutableList
+
+import org.prolog4j._
+
+//import org.exist.storage.DBBroker
+
+import org.xmldb.api.base.{ Resource => XmlDbRrsc, _}
+import org.xmldb.api.modules._
+import org.xmldb.api._
+
+//import org.exist.util.serializer.SAXSerializer
+//import org.exist.util.serializer.SerializerPool
+
+import com.thoughtworks.xstream.XStream
+import com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver
+
+import javax.xml.transform.OutputKeys
+import java.util.Properties
+import java.net.URI
+import java.io.File
+import java.io.FileInputStream
+import java.io.OutputStreamWriter
+
+trait CnxnBasedLoggingScope[M[_],Namespace,Var,Tag,Value]
+extends HistoricalContextScope[M,CnxnCtxtLabel[Namespace,Var,Tag]] {
+  trait CnxnWriter[A] extends WriterM[A] {
+  }
+}
