98a77db8530c874dfd339d7099d90b41afb1a817
Meredith Gregory <lgreg.meredith@biosimilarity.com>
2012-05-21 13:09:44 -0700

    Added the automatically importing the records file

--- specialk/scripts/alarm-me.scala
+++ specialk/scripts/alarm-me.scala
@@ -16,11 +16,25 @@ import CnxnConversionStringScope._
 
 import scala.util.continuations._
 
+import org.basex.core._
+import org.basex.core.cmd.Open
+import org.basex.core.cmd.Add
+import org.basex.core.cmd.CreateDB
+
 object SoYouThinkYouCanTest {
   def setup() : PersistedMonadicKVDBNode[PersistedKVDBNodeRequest,PersistedKVDBNodeResponse]
   = {
     PersistedMonadicKVDBCollectD.loadData()
-    kvdb( "/" + ( PersistedMonadicKVDBCollectD.importData()( 0 ).replace( ".xml", "" ) ) )
+    val recordsFileName = 
+      PersistedMonadicKVDBCollectD.importData()( 0 )
+    val recordsFileNameRoot = recordsFileName.replace( ".xml", "" )
+    val node = kvdb( "/" + ( recordsFileNameRoot ) )
+    val clientSession = node.cache.clientSessionFromConfig
+    val currentDir = new java.io.File(".").getAbsolutePath()
+    val recordsFullFileName = currentDir.replace( "/.", "/" + recordsFileName )
+    clientSession.execute( new CreateDB( recordsFileNameRoot ) )
+    clientSession.execute( new Add( recordsFullFileName ) )
+    node
   }
   def go( kvdb : PersistedMonadicKVDBNode[PersistedKVDBNodeRequest,PersistedKVDBNodeResponse] ) = {
     val pvT1 = partialCaseClassDerivative( pvOne, List( ( "time", "t" ), ( "host", "host" ) ) )
