316ca12837c8703485ed3c0a73d7f5c552f5a1e4
Jason Klassen <jason.klassen@protegra.com>
2012-07-03 13:07:12 -0500

    start of tests working with Being.AgentKVDBNode[ PersistedKVDBNodeRequest, PersistedKVDBNodeResponse ] and persisted gets

--- agent-service/AgentServicesStore/src/test/scala/com/protegra/agentservicesstore/KvdbPlatformAgentSingleTest.scala
+++ agent-service/AgentServicesStore/src/test/scala/com/protegra/agentservicesstore/KvdbPlatformAgentSingleTest.scala
@@ -7,7 +7,7 @@
 // ------------------------------------------------------------------------
 
 package com.protegra.agentservicesstore
-
+ 
 import org.specs._
 import org.specs.util._
 import org.specs.runner.JUnit4
@@ -16,6 +16,7 @@ import org.specs.runner.ConsoleRunner
 import com.biosimilarity.lift.model.store._
 import com.protegra.agentservicesstore.extensions.StringExtensions._
 import com.protegra.agentservicesstore.extensions.ResourceExtensions._
+import com.protegra.agentservicesstore.extensions.URIExtensions._
 
 import scala.util.continuations._
 
@@ -38,94 +39,81 @@ object KvdbPlatformAgentSingleTestSpecsRunner
   extends ConsoleRunner(KvdbPlatformAgentSingleTestSpecs)
 
 object KvdbPlatformAgentSingleTestSpecs //extends KvdbPlatformAgentBase
-extends Specification
-with Timeouts
+  extends Specification
+  with SpecsKVDBHelpers
+  with Timeouts
+  with RabbitTestSetup
 {
   val timeoutBetween = 0
 
   val sourceAddress = "127.0.0.1"
-  val acquaintanceAddresses = List[URI]()
-  val _localQ = AgentKVDBNodeFactory.ptToMany(sourceAddress.toURI, acquaintanceAddresses)
-//  val _localQ = createJunction(sourceAddress.toURM, acquaintanceAddresses)
-//  def createJunction(sourceAddress: URM, acquaintanceAddresses: List[URM]): PartitionedStringMGJ = new PartitionedStringMGJ(sourceAddress, acquaintanceAddresses, None)
+  val acquaintanceAddresses = List[ URI ]()
+//    val _localQ = AgentKVDBNodeFactory.ptToMany(sourceAddress.toURI, acquaintanceAddresses)
+  val _localQ: Being.AgentKVDBNode[ PersistedKVDBNodeRequest, PersistedKVDBNodeResponse ] = AgentKVDBNodeFactory.ptToMany(sourceAddress.toURI, acquaintanceAddresses)
+  //  val _localQ = createJunction(sourceAddress.toURM, acquaintanceAddresses)
+  //  def createJunction(sourceAddress: URM, acquaintanceAddresses: List[URM]): PartitionedStringMGJ = new PartitionedStringMGJ(sourceAddress, acquaintanceAddresses, None)
 
 
-//  testMessaging(_localQ, _localQ)
-//  testWildcardWithPut(_localQ, _localQ)
-//  testWildcardWithStore(_localQ, _localQ)
-//  testWildcardWithCursor(_localQ, _localQ)
-//  testWildcardWithCursorBefore(_localQ, _localQ)
+  //  testMessaging(_localQ, _localQ)
+  //  testWildcardWithPut(_localQ, _localQ)
+  //  testWildcardWithStore(_localQ, _localQ)
+  //  testWildcardWithCursor(_localQ, _localQ)
+  //  testWildcardWithCursorBefore(_localQ, _localQ)
 
   val sourceId = UUID.randomUUID
   val targetId = sourceId
   val cnxn = new AgentCnxn(sourceId.toString.toURI, "", targetId.toString.toURI)
   val cnxnRandom = new AgentCnxn("Random".toURI, "", UUID.randomUUID.toString.toURI)
 
-//  "MGJ With Acquaintances" should {
-//    "have no implicit error" in {
-//      val sourceAddress = "127.0.0.1"
-//      val acquaintanceAddresses = List[URM](sourceAddress.toURM)
-//      val testQ = new PartitionedStringMGJ(sourceAddress.toURM, acquaintanceAddresses, None)
-//      testQ.agentTwistedPairs
-//
-//      //did not crash by this point
-//    }
-//  }
+  "Get" should {
+    "not find when key is missing" in {
+      val key = "contentResponse(nonexistingGet(\"not here\"))".toLabel
+      getMustBe("")(_localQ, cnxn, key)
+    }
+  }
 
-//  "Get" should {
-//    "not find when key is missing" in {
-//      val key = "contentResponse(nonexistingGet(\"not here\"))".toLabel
-//      getMustBe("")(_localQ, cnxn, key)
-//    }
-//  }
-//
-//  "Fetch" should {
-//    "not find when key is missing" in {
-//      val key = "contentResponse(nonexistingFetch(\"not here\"))".toLabel
-//      fetchMustBe("")(_localQ, cnxn, key)
-//    }
-//  }
+  "Fetch" should {
+    "not find when key is missing" in {
+      val key = "contentResponse(nonexistingFetch(\"not here\"))".toLabel
+      fetchMustBe("")(_localQ, cnxn, key)
+    }
+  }
 
   //ISSUE 37: different labels get1get2 put1put1 keep going down alternating gets
   "2 Cached Get/Put" should {
 
     Thread.sleep(timeoutBetween)
     "retrieve" in {
-//      val writer = new PartitionedStringMGJ("127.0.0.1".toURM, List[URM](), None)
-      //AgentKVDBNodeFactory.ptToMany("127.0.0.1".toURI, List[URI]())
-      val Right( ( writerClnt, writerSrvr ) ) = 
-	setup[PersistedKVDBNodeRequest,PersistedKVDBNodeResponse]( "localhost", 5672, "localhost", 5672 )( true )
+      val resultsDb: Being.AgentKVDBNode[ PersistedKVDBNodeRequest, PersistedKVDBNodeResponse ] = AgentKVDBNodeFactory.ptToMany("127.0.0.1".toURI.withPort(RABBIT_PORT_TEST_RESULTS_DB), List[ URI ]())
+      val testId = UUID.randomUUID().toString()
+      val cnxnTest = new AgentCnxn(( "TestDB" + testId ).toURI, "", ( "TestDB" + testId ).toURI)
+
+      val writer = AgentKVDBNodeFactory.ptToMany("127.0.0.1".toURI, List[ URI ]())
 
       val lblGlobalRequest = "globalRequest(\"email\")".toLabel
       val lblGlobalResponse = "globalResponse(\"email\")".toLabel
 
-      val cnxnGlobal = new AgentCnxn("Global".toURI, "", "Global".toURI)
+      val globalId = UUID.randomUUID().toString()
+      val cnxnGlobal = new AgentCnxn(( "Global" + globalId ).toURI, "", ( "Global" + globalId ).toURI)
 
-      println("ready")
-
-      val resultQ = new LinkedBlockingQueue[String]
-
-      def listenGlobalRequest: Unit =
+      def listenGlobalRequest(): Unit =
       {
         reset {
-          for (e <- writerClnt.get(cnxnGlobal)(lblGlobalRequest)) {
-            if (e != None) {
-              println("************* REQUEST RECEIVED : " + e.dispatch)
-              resultQ.add(e.dispatch)
-
+          for ( e <- writer.get(cnxnGlobal)(lblGlobalRequest) ) {
+            if ( e != None ) {
+              val lblResult = ( "result(\"" + UUID.randomUUID() + "\")" ).toLabel
+              reset {resultsDb.put(cnxnTest)(lblResult, e.dispatch)}
               listenGlobalRequest
             }
           }
         }
       }
 
-      listenGlobalRequest
-
       def listenGlobalResponse: Unit =
       {
         reset {
-          for (e <- writerClnt.get(cnxnGlobal)(lblGlobalResponse)) {
-            if (e != None) {
+          for ( e <- writer.get(cnxnGlobal)(lblGlobalResponse) ) {
+            if ( e != None ) {
               println("************* RESPONSE RECEIVED : " + e.dispatch)
               // No message should be received on this label
               fail("Response was received, but should not have been.")
@@ -139,11 +127,13 @@ with Timeouts
       listenGlobalResponse
 
       val valueGlobalRequest = "START THE GLOBAL REQUEST"
-      reset {writerClnt.put(cnxnGlobal)(lblGlobalRequest, Ground(valueGlobalRequest + ": 1"))}
+      listenGlobalRequest
+      reset {writer.put(cnxnGlobal)(lblGlobalRequest, Ground(valueGlobalRequest + ": 1"))}
       Thread.sleep(TIMEOUT_MED)
-      reset {writerClnt.put(cnxnGlobal)(lblGlobalRequest, Ground(valueGlobalRequest + ": 2"))}
+      reset {writer.put(cnxnGlobal)(lblGlobalRequest, Ground(valueGlobalRequest + ": 2"))}
 
-      resultQ.size() must be_==(2).eventually(10, TIMEOUT_EVENTUALLY)
+      val strResultSearch = "result(_)"
+      countMustBe(2)(resultsDb, cnxnTest, strResultSearch)
     }
   }
 }
