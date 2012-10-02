116b6c4c1b7bfc4f137a3bb309a3d468ebd81ed0
jason klassen <jason.klassen@protegra.com>
2012-10-02 02:29:07 -0500

    adding a hook into the invitation protocol to send a notification to the user

--- agent-service/AgentServices-Core/src/main/scala/com/protegra_ati/agentservices/core/messages/invitation/InvitationRequestSetConsumer.scala
+++ agent-service/AgentServices-Core/src/main/scala/com/protegra_ati/agentservices/core/messages/invitation/InvitationRequestSetConsumer.scala
@@ -124,39 +124,13 @@ trait InvitationRequestSetConsumer
     val persistedInvitationRequestMessage = new PersistedMessage[ InvitationRequest ](inviteRequest)
     store(_dbQ, selfConnection.writeCnxn, persistedInvitationRequestMessage.toStoreKey, Serializer.serialize[ PersistedMessage[ InvitationRequest ] ](persistedInvitationRequestMessage))
     val profileQuery = new Profile()
-    fetch[ Data ](_dbQ, selfConnection.writeCnxn, profileQuery.toSearchKey, notificationHandler(_: AgentCnxnProxy, _: Data, inviteRequest))
+    fetch[ Data ](_dbQ, selfConnection.writeCnxn, profileQuery.toSearchKey, invitationRequestNotificationHandler(_: AgentCnxnProxy, _: Data, inviteRequest))
 
   }
 
-  protected def notificationHandler(cnxn: AgentCnxnProxy, user: Data, inviteRequest: InvitationRequest) =
+  protected def invitationRequestNotificationHandler(cnxn: AgentCnxnProxy, user: Data, inviteRequest: InvitationRequest) =
   {
-//    user match {
-//      case p: Profile => {
-//        try {
-//          val locale: Locale = new Locale(p.localeCode)
-//          val resourceBundle = ResourceManager.getResourceBundle(locale)
-//          val templateResourceBundle = ResourceManager.getTemplateResourceBundle(locale)
-//          val messageTemplate = templateResourceBundle.getString("connection_request_notification_email")
-//          val formatter: DateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-//          // replacement logic for variables in a template
-//          var valuesMap: Map[ String, String ] = Map[ String, String ]()
-//          valuesMap += ( "TIMESTAMP" -> formatter.format(new Date) )
-//          valuesMap += ( "USER" -> p.firstName )
-//          valuesMap += ( "CONNECTION_REQUEST_URL" -> ConfigurationManager.getConfigurationManager().getString("notification.email.connection.request.url") )
-//          valuesMap += ( "CONNECTION_REQUEST_UID" -> inviteRequest.ids.conversationId )
-//          val sub: StrSubstitutor = new StrSubstitutor(valuesMap)
-//          val message: String = sub.replace(messageTemplate);
-//          sendNotification(Email.createSystemEmail(p, resourceBundle.getString("connection.request.notification.email.system.subject"), message, "text/html"))
-//          report("send notification about connection request to " + p.firstName + " " + p.lastName, Severity.Trace)
-//        } catch {
-//          case e: Exception => {
-//            report("unable to send notification due to " + e.getCause.toString, Severity.Error)
-//            e.printStackTrace()
-//          }
-//          case _ => {}
-//        }
-//      }
-//      case _ => {}
-//    }
+    //email, sms, however you want to notify
+    //also a hook to override in other implementations
   }
 }
