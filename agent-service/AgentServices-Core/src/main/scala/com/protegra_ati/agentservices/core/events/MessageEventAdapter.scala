c3a11fec4a8e2f42268871a894788d8c4ff9d15b
Jason Klassen <jason.klassen@protegra.com>
2012-11-23 11:15:09 -0600

    removing serializable from the adapter

--- agent-service/AgentServices-Core/src/main/scala/com/protegra_ati/agentservices/core/events/MessageEventAdapter.scala
+++ agent-service/AgentServices-Core/src/main/scala/com/protegra_ati/agentservices/core/events/MessageEventAdapter.scala
@@ -4,9 +4,11 @@
  */
 
 package com.protegra_ati.agentservices.core.events
+//these are used as anonymous inner classes in java, should not be  with java.io.Serializable
+//even though compile time allows run time will try to serialize whole outer object that may not be serializable
 
 //strictly for java api compatibility, using the trait directly is compiled as interface
-class MessageEventAdapter(_eventTag:String) extends MessageEventListener with java.io.Serializable {
+class MessageEventAdapter(_eventTag:String) extends MessageEventListener{
   def this() = this("")
   var eventTag: String = _eventTag
 }
