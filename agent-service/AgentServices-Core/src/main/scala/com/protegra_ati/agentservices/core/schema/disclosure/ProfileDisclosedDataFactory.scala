d5a4aecb3f249e5466c94e6141c497eca44dd02b
renns <raphael.enns@protegra.com>
2013-01-11 16:43:01 -0600

    Issue 390 - Update trust level defaults.

--- agent-service/AgentServices-Core/src/main/scala/com/protegra_ati/agentservices/core/schema/disclosure/ProfileDisclosedDataFactory.scala
+++ agent-service/AgentServices-Core/src/main/scala/com/protegra_ati/agentservices/core/schema/disclosure/ProfileDisclosedDataFactory.scala
@@ -11,7 +11,7 @@ class ProfileDisclosedDataFactory private() extends AbstractDisclosedDataFactory
   private val defaultDisclosedDataCollection: scala.collection.immutable.HashMap[ TrustLevel.Value, DisclosedData[ Profile ] ] =
     HashMap[ TrustLevel.Value, DisclosedData[ Profile ] ](
       TrustLevel.Full -> DisclosedData[ Profile ](classOf[ Profile ], TrustLevel.Full.toString, "id,localeCode,firstName,lastName,description,emailAddress,country,region,city,postalCode,website,imageHashCode"),
-      TrustLevel.Basic -> DisclosedData[ Profile ](classOf[ Profile ], TrustLevel.Basic.toString, "id,localeCode,firstName,lastName,country,imageHashCode"),
+      TrustLevel.Basic -> DisclosedData[ Profile ](classOf[ Profile ], TrustLevel.Basic.toString, "id,localeCode,firstName,lastName,description,country,imageHashCode"),
       TrustLevel.Introduced -> DisclosedData[ Profile ](classOf[ Profile ], TrustLevel.Introduced.toString, "id,localeCode,lastName,country"),
       TrustLevel.Empty -> DisclosedData[ Profile ](classOf[ Profile ], TrustLevel.Empty.toString, ""))
 
