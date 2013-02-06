1a1303b1f4bf24a332ab9b744b0a7590ef8bc974
Meredith Gregory <lgreg.meredith@biosimilarity.com>
2013-02-06 14:26:00 -0800

    Committing changes from extreme programming session

--- specialk/src/main/scala/com/biosimilarity/lift/model/store/CnxnMongo.scala
+++ specialk/src/main/scala/com/biosimilarity/lift/model/store/CnxnMongo.scala
@@ -37,17 +37,129 @@ trait CnxnMongoQuery[Namespace,Var,Tag] {
 	// BUGBUG : lgm -- incorrect semantics
 	( v + "" ) $exists true
       }
-      case CnxnCtxtBranch( functor, factuals ) => {
-	val functorStr = functor + ""	
-	val factArray = new Array[DBObject]( factuals.length )
-	for( i <- ( 0 to factuals.length - 1 ) ) {
-	  factArray( i ) = toMongoQuery( factuals( i ) )
+      case ccb@CnxnCtxtBranch( functor, factuals ) => {
+	def loop( ccl : CnxnCtxtLabel[Namespace,Var,Tag] ) : List[String] = {
+	  ccl match {
+	    case CnxnCtxtLeaf( Left( tag ) ) => {
+	      List( ( tag + "" ) )
+	    }
+	    case CnxnCtxtLeaf( Right( "_" ) ) => {
+	      // BUGBUG : lgm -- incorrect semantics
+	      List( "" )
+	    }
+	    case CnxnCtxtLeaf( Right( v ) ) => {
+	      // BUGBUG : lgm -- incorrect semantics
+	      List( ( v + "" ) )
+	    }
+	    case CnxnCtxtBranch( innerFunctor, innerFactuals ) => {
+	      ( List[String]() /: innerFactuals )(
+		{
+		  ( acc, e ) => {
+		    (
+		      acc
+		      ++
+		      (
+			loop( e ).map(
+			  {
+			    ( path ) => {
+			      path match {
+				case "" => innerFunctor + ""
+				case _ => innerFunctor + "." + path
+			      }
+			    }
+			  }
+			)
+		      )
+		    )
+		  }
+		}
+	      )
+	    }
+	  }
 	}
-	MongoDBObject( functorStr -> MongoDBObject( "fields" -> factArray ) )
+
+	( MongoDBObject.empty /: loop( ccb ) )(
+	  {
+	    ( acc, e ) => {
+	      acc ++ ( e $exists true )
+	    }
+	  }
+	)
+
+      }
+    }
+  }  
+}
+
+trait CnxnMongoObject[Namespace,Var,Tag] {
+  self : CnxnCtxtInjector[Namespace,Var,Tag]
+         with CnxnString[Namespace,Var,Tag]
+	 with Blobify with UUIDOps =>
+
+  def toMongoObjectOuter(
+    cnxn : CnxnCtxtLabel[Namespace,Var,Tag]
+  )  : DBObject = {
+    cnxn match {
+      case CnxnCtxtLeaf( Left( tag ) ) => {
+	MongoDBObject( ( tag + "" ) -> 1 )
+      }
+      case CnxnCtxtLeaf( Right( v ) ) => {
+	// BUGBUG : lgm -- incorrect semantics
+	MongoDBObject( ( v + "" ) -> 1 )
+      }
+      case CnxnCtxtBranch( functor, factuals ) => {
+	MongoDBObject( ( functor + "" ) -> toMongoObjectInner( cnxn ) )
       }
     }
   }
+
+  def toMongoObjectInner(
+    cnxn : CnxnCtxtLabel[Namespace,Var,Tag]
+  ) : DBObject = {
+    cnxn match {
+      case CnxnCtxtLeaf( Left( tag ) ) => {
+	MongoDBObject( ( tag + "" ) -> 1 )
+      }
+      case CnxnCtxtLeaf( Right( v ) ) => {
+	// BUGBUG : lgm -- incorrect semantics
+	MongoDBObject( ( v + "" ) -> 1 )
+      }
+      case CnxnCtxtBranch( functor, factuals ) => {
+	val functorStr = functor + ""	
+	val factArray = new Array[Any]( factuals.length )
+	
+	MongoDBObject(
+	  for( fact <- factuals ) yield {
+	    fact match {
+	      case CnxnCtxtBranch( innerFunctor, innerFactuals ) => {
+		( innerFunctor + "" ) -> toMongoObjectInner( fact )
+	      }
+	      case CnxnCtxtLeaf( Left( tag ) ) => {
+		( tag + "" ) -> 1
+	      }
+	      case CnxnCtxtLeaf( Right( v ) ) => {
+		// BUGBUG : lgm -- incorrect semantics
+		( v + "" ) -> 1
+	      }
+	    }
+	  }
+	)
+      }
+    }
+  }  
 }
 
 package usage {
+  object MyCnxnMongoQuery extends CnxnMongoQuery[String,String,String]
+    with CnxnMongoObject[String,String,String]
+    with CnxnCtxtInjector[String,String,String]
+    with CnxnString[String,String,String]
+    with Blobify with UUIDOps {
+      implicit def fromString( s : String ) : CnxnCtxtLabel[String,String,String] = {
+	fromCaseClassInstanceString( s ) match {
+	  case Some( ccl ) => ccl.asInstanceOf[CnxnCtxtLabel[String,String,String]];
+	  case None => throw new Exception( "failed to parse" )
+	}
+      }
+    }
 }
