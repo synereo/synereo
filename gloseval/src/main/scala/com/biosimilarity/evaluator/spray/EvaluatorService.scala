0084230ea3926830c0fa833f5de672bdefb0c137
Mike Stay <metaweta@gmail.com>
2013-03-07 16:11:43 -0700

    Mock up handlers for initializeSessionRequest, evalRequest, and closeSessionRequest.

--- gloseval/src/main/scala/com/biosimilarity/evaluator/spray/EvaluatorService.scala
+++ gloseval/src/main/scala/com/biosimilarity/evaluator/spray/EvaluatorService.scala
@@ -8,6 +8,8 @@ import spray.routing._
 import spray.http._
 import MediaTypes._
 import HttpMethods._
+import org.json4s._
+import org.json4s.native.JsonMethods._
 
 
 // we don't implement our route structure directly in the service actor because
@@ -70,18 +72,36 @@ class EvaluatorServiceActor extends Actor with SprayActorLogging {
     case HttpRequest(GET, "/timeout/timeout", _, _, _) =>
       log.info("Dropping request, triggering a timeout")
 
-    case HttpRequest(POST, uri, hdrs, entity, _) => {
-      println( "uri: " + uri );
-      println( "hdrs: " + hdrs );      
-      entity.foreach(
-	( cntntType : ContentType, bytes : Array[Byte] ) => {
-	  println( "bytes: " + bytes )
-	}
-      )
-      sender ! stockMsgResponse
+    case HttpRequest(POST, uri, hdrs, HttpBody(cntntType, body), _) => {
+      // println( "uri: " + uri );
+      // println( "hdrs: " + hdrs );      
+      val bodyStr = new String(body);
+      // println( "content type: " + cntntType + "\nbody:\n" + bodyStr )
+
+      try {
+        val json = parse(bodyStr)
+
+        val msgType = json \ "msgType" match {
+          case JString(s) => s
+          case _ => throw MalformedRequestException()
+        }
+
+        // Dispatch on messageType
+        msgType match {
+          case "initializeSessionRequest" => initializeSessionRequest(json)
+          case "evalRequest" => evalRequest(json)
+          case "closeSessionRequest" => closeSessionRequest(json)
+          case _ => sender ! HttpResponse(404, "Unknown message type: " + msgType + "\n")
+        }
+      } catch {
+        case InitializeSessionException(agentURI, message) => sender ! initializeSessionError(agentURI, message)
+        case EvalException(sessionURI) => sender ! evalError(sessionURI)
+        case CloseSessionException(sessionURI, message) => sender ! closeSessionError(sessionURI, message)
+        case _ : Throwable => sender ! HttpResponse(404, "Malformed request\n")
+      }
     }
 
-    case _: HttpRequest => sender ! HttpResponse(404, "Unknown resource!")
+    case _: HttpRequest => sender ! HttpResponse(404, "Unknown resource!\n")
 
     // case Timedout(HttpRequest(_, "/timeout/timeout", _, _, _)) =>
 //       log.info("Dropping Timeout message")
@@ -90,7 +110,142 @@ class EvaluatorServiceActor extends Actor with SprayActorLogging {
 //       sender ! HttpResponse(500, "The " + request.method + " request to '" + request.uri + "' has timed out...")
   }
 
+  ////////////// message handlers //////////////
+
+  def initializeSessionRequest(json: JValue) = {
+    val agentURI = json \ "content" \ "agentURI" match {
+      case JString(s) => s
+      case _ => throw MalformedRequestException()
+    }
+    val uri = new java.net.URI(agentURI)
+    if (uri.getScheme() != "agent") {
+      throw InitializeSessionException(agentURI, "Unrecognized scheme")
+    }
+    if (uri.getUserInfo() != "George.Costanza:Bosco") {
+      println(uri.getAuthority())
+      throw InitializeSessionException(agentURI, "Unrecognized user/password combination")
+    }
+    if (uri.getPath() != "/TheAgency") {
+      throw InitializeSessionException(agentURI, "Unrecognized agent ID")
+    }
+    
+    sender ! HttpResponse(
+      entity = HttpBody(`application/json`,
+"""{
+  "msgType": "initializeSessionResponse",
+  "content": {
+    "sessionURI": "agent-session://ArtVandelay@session1",
+    "listOfAliases": [ "George Costanza", "Lord of the Idiots", "Biff Loman", "Gammy" ],
+    "defaultAlias": "George Costanza",
+    "listOfLabels": [ "Locations", "Media", "Interests" ],
+    "listOfCnxns": [ "Elaine Benes", "Cosmo Kramer", "Newman", "Tom's Restaurant"],
+    "lastActiveFilter": ""
+  }
+}
+"""
+      )
+    )
+  }
+
+
+
+
+  def evalRequest(json: JValue) = {
+    val sessionURI = json \ "content" \ "sessionURI" match {
+      case JString(s) => s
+      case _ => throw MalformedRequestException()
+    }
+    if (sessionURI != "agent-session://ArtVandelay@session1") {
+      throw EvalException(sessionURI)
+    }
+    
+    sender ! HttpResponse(
+      entity = HttpBody(`application/json`,
+"""{
+  "msgType": "evalComplete",
+  "content": {
+    "sessionURI": "agent-session://ArtVandelay@session1",
+    "pageOfPosts": []
+  }
+}
+"""
+      )
+    )
+  }
+
+
+
+
+  def closeSessionRequest(json: JValue) = {
+    val sessionURI = json \ "content" \ "sessionURI" match {
+      case JString(s) => s
+      case _ => throw MalformedRequestException()
+    }
+    if (sessionURI != "agent-session://ArtVandelay@session1") {
+      throw CloseSessionException(sessionURI, "Unknown session")
+    }
+    
+    sender ! HttpResponse(
+      entity = HttpBody(`application/json`,
+"""{
+  "msgType": "closeSessionResponse",
+  "content": {
+    "sessionURI": "agent-session://ArtVandelay@session1",
+  }
+}
+"""
+      )
+    )
+  }
+
+
+
+
   ////////////// helpers //////////////
+  
+  case class MalformedRequestException() extends Exception
+  case class InitializeSessionException(agentURI: String, message: String) extends Exception
+  case class EvalException(sessionURI: String) extends Exception
+  case class CloseSessionException(sessionURI: String, message: String) extends Exception
+  
+  def initializeSessionError(agentURI:String, message: String) = HttpResponse(
+    entity = HttpBody(`application/json`,
+"""{
+  "msgType": "initializeSessionError",
+  "content": {
+    "agentURI": """" + agentURI + """",
+    "reason" : """" + message + """"
+  }
+}
+"""
+    )
+  )
+
+  def evalError(sessionURI: String) = HttpResponse(
+    entity = HttpBody(`application/json`,
+"""{
+  "msgType": "evalError",
+  "content": {
+    "sessionURI": """" + sessionURI + """"
+  }
+}
+"""
+    )
+  )
+
+  def closeSessionError(sessionURI: String, message: String) = HttpResponse(
+    entity = HttpBody(`application/json`,
+"""{
+  "msgType": "closeSessionError",
+  "content": {
+    "sessionURI": """" + sessionURI + """",
+    "reason" : """" + message + """"
+  }
+}
+"""
+    )
+  )
+
 
   lazy val index = HttpResponse(
     entity = HttpBody(`text/html`,
@@ -112,7 +267,7 @@ class EvaluatorServiceActor extends Actor with SprayActorLogging {
 
   lazy val stockMsgResponse = HttpResponse(
     entity = HttpBody(`application/json`,
-      "{ \"msgType\" : \"evalComplete\", \"contents\" : { \"sessionURI\" : \"agent-session://myLovelySession/1234, \" \"pageOfPosts\" : [] }"
+      "{ \"msgType\" : \"evalComplete\", \"contents\" : { \"sessionURI\" : \"agent-session://myLovelySession/1234, \" \"pageOfPosts\" : [] }\n"
     )
   )
 
