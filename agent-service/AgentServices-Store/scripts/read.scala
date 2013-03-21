ad5288ed2da24c6920b649acaef6fda3f2cc91d5
jason klassen <jason.klassen@protegra.com>
2013-03-21 13:49:07 -0500

    revd version, script for delete

--- agent-service/AgentServices-Store/scripts/read.scala
+++ agent-service/AgentServices-Store/scripts/read.scala
@@ -1,4 +1,3 @@
-
 import scala.util.continuations._
 import java.net.URI
 
@@ -14,26 +13,23 @@ import com.protegra_ati.agentservices.store.mongo.usage._
 val sourceAddress = "127.0.0.1".toURI
 val acquaintanceAddresses = List[ URI ]()
 val configFileName = Some("db_store.conf")
-//val node = AgentKVDBNodeFactory.ptToMany(sourceAddress, acquaintanceAddresses)(configFileName)
 val space = AgentUseCase(configFileName)
 val node = space.createNode(sourceAddress, acquaintanceAddresses, configFileName)
 
-val cnxn = new AgentCnxn("StoreTest".toURI, "", "Jason".toURI)
+val cnxn = new AgentCnxn("DeleteTest".toURI, "", "Jason".toURI)
 
 val lbl = ( "contentChannel(\"123\")" ).toLabel
 val value = "testtest"
-//reset { node.put(cnxn)(lbl, Ground(value)) }
 
 node.store(cnxn)(lbl, Ground(value))
 
 val lblSearch = "contentChannel(_)".toLabel
 
-//reset { for( e <- node.subscribe(cnxn)( lblSearch ) ) { println( "received: " + e) } }
 reset { for( e <- node.read(cnxn)( lblSearch ) ) { println( "received: " + e) } }
 
+node.delete(cnxn)(lbl)
 
+node.delete(cnxn)(lblSearch)
 
-reset { for( e <- node.fetch(cnxn)( lblSearch ) ) { println( "received: " + e.dispatch ) } }
-
-
+reset { for( e <- node.read(cnxn)( lblSearch ) ) { println( "received: " + e) } }
 
