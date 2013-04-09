2c78235df3223d5918becb03348cf396dcdeb8c2
Paul Spratt <paspratt@gmail.com>
2013-04-09 09:15:23 -0500

    - run-ui batch file modified to take in plugin name on command line. - Added a specialized exception for invalid/uninitialized session parameters. - Add the initial (empty) CreateLAbelPlugin.

---
+++ agent-service/IAServices/src/main/scala/com/ati/iaservices/recipes/SessionInvalidException.scala
@@ -0,0 +1,11 @@
+package com.ati.iaservices.recipes
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pspratt
+ * Date: 08/04/13
+ * Time: 5:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class SessionInvalidException(msg:String) extends Exception(msg) {
+}
