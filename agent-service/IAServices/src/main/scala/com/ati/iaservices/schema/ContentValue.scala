42421c403e08701e5ed6af24005d0f1ccc78ad8d
bkraitberg <bkraitberg@gmail.com>
2013-04-12 14:31:47 -0500

    Fix merge error

--- agent-service/IAServices/src/main/scala/com/ati/iaservices/schema/ContentValue.scala
+++ agent-service/IAServices/src/main/scala/com/ati/iaservices/schema/ContentValue.scala
@@ -1,13 +1,11 @@
 package com.ati.iaservices.schema
 
-import com.protegra_ati.agentservices.core.util.serializer.UseJavaIOSerialization
 import com.protegra_ati.agentservices.store.schema.KVDBSerializable
 import com.protegra_ati.agentservices.store.util.Reporting
 
 abstract class ContentValue()
   extends Serializable
   with KVDBSerializable
-  with UseJavaIOSerialization
   with Reporting
 {
 
