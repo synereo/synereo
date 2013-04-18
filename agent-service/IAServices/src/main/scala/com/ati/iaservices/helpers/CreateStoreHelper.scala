66c6bb9f67868b97937f7f02dd8f082785186e40
bkraitberg <bkraitberg@gmail.com>
2013-04-18 13:27:52 -0500

    Issue 77: IAServices - Determine why db_store.conf is still required
    db_store.conf and related config files are still required to create an AgentHostStorePlatformAgent.
    
    Added files back in to source control, along with check in CreateStoreHelper for file existence.

--- agent-service/IAServices/src/main/scala/com/ati/iaservices/helpers/CreateStoreHelper.scala
+++ agent-service/IAServices/src/main/scala/com/ati/iaservices/helpers/CreateStoreHelper.scala
@@ -5,6 +5,9 @@ import com.ati.iaservices.recipes.LauncherPluginUtil
 
 class CreateStoreHelper {
   final val STORE_CONFIG = "init_store.conf"
+  final val DB_STORE_CONFIG = "db_store.conf"
+  final val DB_STORE_DB_CONFIG = "db_store_db.conf"
+  final val DB_STORE_PUBLIC_CONFIG = "db_store_public.conf"
   final val LOG_KVDB_CONFIG = "log.conf"
   final val LOG_KVDB_PROPERTIES = "log.properties"
   final val LOG_AGENTSERVICES_CONFIG = "log_agentservices.conf"
@@ -13,6 +16,9 @@ class CreateStoreHelper {
   //refactor this into core
   def checkAllStoreConfigFiles() = {
     LauncherPluginUtil.configFileExists(STORE_CONFIG)
+    LauncherPluginUtil.configFileExists(DB_STORE_CONFIG)
+    LauncherPluginUtil.configFileExists(DB_STORE_DB_CONFIG)
+    LauncherPluginUtil.configFileExists(DB_STORE_PUBLIC_CONFIG)
     LauncherPluginUtil.configFileExists(LOG_KVDB_CONFIG)
     LauncherPluginUtil.configFileExists(LOG_KVDB_PROPERTIES)
     LauncherPluginUtil.configFileExists(LOG_AGENTSERVICES_CONFIG)
