e5cc04ce307f834c15589d33edc8f9d11336826b
Raphael Enns <raphael.enns@protegra.com>
2013-04-23 14:22:01 -0500

    Added DSL PA to IAServices.

--- agent-service/IAServices/src/main/scala/com/ati/iaservices/helpers/CreateDslHelper.scala
+++ agent-service/IAServices/src/main/scala/com/ati/iaservices/helpers/CreateDslHelper.scala
@@ -1,29 +1,29 @@
 package com.ati.iaservices.helpers
 
+import com.ati.iaservices.platformagents.AgentHostDslPlatformAgent
 import com.ati.iaservices.recipes.LauncherPluginUtil
-import com.protegra_ati.agentservices.core.platformagents.AgentHostUIPlatformAgent
 import com.protegra_ati.agentservices.core.util.ConfigurationManager
 
-class CreateUIHelper {
-  final val UI_CONFIG = "init_ui.conf"
+class CreateDslHelper {
+  final val DSL_CONFIG = "init_ui.conf"
   final val LOG_KVDB_CONFIG = "log.conf"
   final val LOG_KVDB_PROPERTIES = "log.properties"
   final val LOG_AGENTSERVICES_CONFIG = "log_agentservices.conf"
   final val LOG_AGENTSERVICES_PROPERTIES = "log_agentservices.properties"
 
   def checkAllStoreConfigFiles() {
-    LauncherPluginUtil.configFileExists(UI_CONFIG)
+    LauncherPluginUtil.configFileExists(DSL_CONFIG)
     LauncherPluginUtil.configFileExists(LOG_KVDB_CONFIG)
     LauncherPluginUtil.configFileExists(LOG_KVDB_PROPERTIES)
     LauncherPluginUtil.configFileExists(LOG_AGENTSERVICES_CONFIG)
     LauncherPluginUtil.configFileExists(LOG_AGENTSERVICES_PROPERTIES)
   }
 
-  def createUI(): AgentHostUIPlatformAgent = {
-    val ui = new AgentHostUIPlatformAgent()
+  def createDsl(): AgentHostDslPlatformAgent = {
+    val dsl = new AgentHostDslPlatformAgent()
     checkAllStoreConfigFiles()
-    val config = new ConfigurationManager(UI_CONFIG)
-    ui.initFromConfig(config)
-    ui
+    val config = new ConfigurationManager(DSL_CONFIG)
+    dsl.initFromConfig(config)
+    dsl
   }
 }
