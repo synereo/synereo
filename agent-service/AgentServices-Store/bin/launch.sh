785b29da76d76755055563f9719cc6c597fe3b67
Meredith Gregory <lgreg.meredith@biosimilarity.com>
2013-05-02 10:41:12 -0700

    Added a simple launch script

---
+++ agent-service/AgentServices-Store/bin/launch.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+java -server -Xms3g -Xmx5g -XX:+PrintCommandLineFlags -cp ../target/agentservices-store-1.2.0.2-IA-jar-with-dependencies.jar com.biosimilarity.evaluator.distribution.diesel.Server
