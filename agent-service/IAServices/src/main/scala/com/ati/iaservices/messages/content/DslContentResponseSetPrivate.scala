9728433c80661ddf43b7ea4356a152a33bbac82a
Raphael Enns <raphael.enns@protegra.com>
2013-05-02 13:25:32 -0500

    Made DSL platform agent send events until we figure out how to bubble up responses.

--- agent-service/IAServices/src/main/scala/com/ati/iaservices/messages/content/DslContentResponseSetPrivate.scala
+++ agent-service/IAServices/src/main/scala/com/ati/iaservices/messages/content/DslContentResponseSetPrivate.scala
@@ -2,7 +2,7 @@ package com.ati.iaservices.messages.content
 
 import com.ati.iaservices.platformagents.AgentHostDslPlatformAgent
 import com.protegra_ati.agentservices.core.messages.content.ContentResponseSetPrivate
-import com.protegra_ati.agentservices.core.messages.{Response, Message}
+import com.protegra_ati.agentservices.core.messages.{EventProducer, Response, Message}
 import com.protegra_ati.agentservices.core.schema.AgentCnxnProxy
 import com.protegra_ati.agentservices.store.util.Severity
 
@@ -15,9 +15,9 @@ trait DslContentResponseSetPrivate {
     report("=================== HANDLE CONTENT RESPONSE ===========, msg is :" + msg.toString, Severity.Debug)
 
     msg match {
-      case x: Message with Response => {
+      case x: Message with EventProducer[Response] => {
         report("in handleContentResponse triggering event for " + x, Severity.Trace)
-        //triggerEvent(x.generateEvent())
+        triggerEvent(x.generateEvent())
       }
       case _ => {}
     }
