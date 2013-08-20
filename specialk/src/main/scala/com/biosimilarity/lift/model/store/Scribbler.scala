035e9e1773065295d299e65d6a9d9d0abbfe5a4e
Meredith Gregory <lgreg.meredith@biosimilarity.com>
2013-08-20 12:19:34 -0700

    Adding code for wrapping continuations with try-catches to print exceptions

---
+++ specialk/src/main/scala/com/biosimilarity/lift/model/store/Scribbler.scala
@@ -0,0 +1,33 @@
+// -*- mode: Scala;-*- 
+// Filename:    Scribble.scala 
+// Authors:     lgm                                                    
+// Creation:    Mon Aug 19 20:32:53 2013 
+// Copyright:   Not supplied 
+// Description: 
+// ------------------------------------------------------------------------
+
+package com.biosimilarity.lift.model.store.scribble
+import scala.util.continuations._ 
+
+trait Scribble[Resource] {
+  def stackTraceToString( t : Throwable ) : String = {
+    val errors : java.io.StringWriter = new java.io.StringWriter()
+    t.printStackTrace( new java.io.PrintWriter( errors ) )
+    errors.toString( )
+  }
+  def wrapWithCatch(
+    sk : Option[Resource] => Unit @suspendable
+  ) : Option[Resource] => Unit @suspendable = {
+    ( optRsrc ) => {
+      try {
+	sk( optRsrc )
+      }
+      catch {
+        case t : Throwable => {
+          println( "unhandled exception : \n" + stackTraceToString( t ) )
+          throw( t )
+        }
+      }
+    }
+  }
+}
