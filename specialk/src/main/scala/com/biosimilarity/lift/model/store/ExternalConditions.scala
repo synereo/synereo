92dd4daaa59b752123137535efdbfd9ac7d1612a
Meredith Gregory <lgreg.meredith@biosimilarity.com>
2013-09-06 15:30:45 -0700

    Added usage package for weak map

--- specialk/src/main/scala/com/biosimilarity/lift/model/store/ExternalConditions.scala
+++ specialk/src/main/scala/com/biosimilarity/lift/model/store/ExternalConditions.scala
@@ -89,6 +89,17 @@ with MapProxy[String,ReflectiveSurfaceT#Specimen]
     Specimen( getType( content ), Some( genExpr ), content )
   }
 
+  implicit def captureSpecimenAsT[T : TypeTag]( content : T ) : Specimen = {
+    Specimen( typeOf[T], None, content.asInstanceOf[AnyRef] )
+  }
+
+  def captureSpecimenAsT[T : TypeTag](
+    content : T,
+    genExpr : ReflectiveSurfaceT#ExprTree
+  ) : Specimen = {
+    Specimen( typeOf[T], Some( genExpr ), content )
+  }
+
   def registerContent( content : AnyRef ) : String = {
     val uuidStr = UUID.randomUUID.toString
     exCndMap += ( uuidStr -> content )
@@ -96,6 +107,14 @@ with MapProxy[String,ReflectiveSurfaceT#Specimen]
   }
   def registerContent( tag : String, content : AnyRef ) : Unit = {
     exCndMap += ( tag -> content )
+  }
+  def registerContentAsT[T : TypeTag]( content : T ) : String = {
+    val uuidStr = UUID.randomUUID.toString
+    exCndMap += ( uuidStr -> captureSpecimenAsT( content ) )
+    uuidStr
+  }
+  def registerContentAsT[T : TypeTag]( tag : String, content : T ) : Unit = {
+    exCndMap += ( tag -> captureSpecimenAsT( content ) )
   }
   def retrieveContent[T : TypeTag](
     tag : String,
@@ -132,3 +151,107 @@ with MapProxy[String,ReflectiveSurfaceT#Specimen]
 
 object ExternalConditions extends ExternalConditionsT
 
+package external.conditions.usage {
+  trait Looky[N] {
+    def b : Boolean
+    def s : String
+    def n : N
+    def r : Option[Looky[N]]
+  }
+  class LookyLoo(
+    override val b : Boolean,
+    override val s : String,
+    override val n : Int,
+    override val r : Option[Looky[Int]]    
+  ) extends Looky[Int]
+  object LookyLoo {
+    def apply(
+      b : Boolean, s : String, n : Int, r : Option[Looky[Int]]
+    ) : LookyLoo = {
+      new LookyLoo( b, s, n, r )
+    }
+    def unapply(
+      ll : LookyLoo
+    ) : Option[( Boolean, String, Int, Option[Looky[Int]] )] = {
+      Some( ( ll.b, ll.s, ll.n, ll.r ) )
+    }
+  }
+  class LookyLooToo(
+    override val b : Boolean,
+    override val s : String,
+    override val n : Double,
+    override val r : Option[Looky[Double]]    
+  ) extends Looky[Double]
+  object LookyLooToo {
+    def apply(
+      b : Boolean, s : String, n : Double, r : Option[Looky[Double]]
+    ) : LookyLooToo = {
+      new LookyLooToo( b, s, n, r )
+    }
+    def unapply(
+      ll : LookyLooToo
+    ) : Option[( Boolean, String, Double, Option[Looky[Double]] )] = {
+      Some( ( ll.b, ll.s, ll.n, ll.r ) )
+    }
+  }
+
+  object TryOut {
+    def addContent() : ( String, String ) = {
+      val ctag1 =
+        ExternalConditions.registerContentAsT[LookyLoo](
+          new LookyLoo( true, "true", 1, None )
+        )
+      val ctag2 =
+        ExternalConditions.registerContentAsT[LookyLooToo](
+          new LookyLooToo( true, "true", 1.0, None )
+        )
+      ( ctag1, ctag2 )
+    }
+    def positiveTests(
+    ) : ( LookyLoo, LookyLooToo, Looky[Int], Looky[Double] ) = {            
+      val ( ctag1, ctag2 ) = addContent()
+      val ll1 : LookyLoo =
+        ExternalConditions.retrieveContent[LookyLoo](
+          ctag1
+        ).getOrElse( null )
+      val ll2 = 
+        ExternalConditions.retrieveContent[LookyLooToo](
+          ctag2
+        ).getOrElse( null )
+      
+      val ll3 =
+        ExternalConditions.retrieveContent[Looky[Int]](
+          ctag1
+        ).getOrElse( null )
+      val ll4 =
+        ExternalConditions.retrieveContent[Looky[Double]](
+          ctag2
+        ).getOrElse( null )
+
+      ( ll1, ll2, ll3, ll4 )
+    }
+    
+    def negativeTests() = {
+      val ( ctag1, ctag2 ) = addContent()
+      val ll1 : LookyLoo =
+        ExternalConditions.retrieveContent(
+          ctag1
+        ).getOrElse( null )
+      val ll2 = 
+        ExternalConditions.retrieveContent(
+          ctag2
+        ).getOrElse( null )
+      
+      val ll3 =
+        ExternalConditions.retrieveContent[Looky[Double]](
+          ctag1
+        ).getOrElse( null )
+      val ll4 =
+        ExternalConditions.retrieveContent[Looky[Int]](
+          ctag2
+        ).getOrElse( null )
+      
+      ( ll1, ll2, ll3, ll4 )
+    }
+  }
+}
