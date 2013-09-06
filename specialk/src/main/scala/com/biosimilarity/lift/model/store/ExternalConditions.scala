9045f010bcafad03aeda496f22f4cdf6878a7aed
Meredith Gregory <lgreg.meredith@biosimilarity.com>
2013-09-06 12:19:35 -0700

    Added basic features of Environment capability

--- specialk/src/main/scala/com/biosimilarity/lift/model/store/ExternalConditions.scala
+++ specialk/src/main/scala/com/biosimilarity/lift/model/store/ExternalConditions.scala
@@ -13,13 +13,15 @@ import scala.concurrent.{Channel => Chan, _}
 import scala.concurrent.cpsops._
 import scala.util.continuations._ 
 import scala.xml._
-import scala.collection.MapProxy
+import scala.collection.mutable.MapProxy
 import scala.collection.mutable.Map
 import scala.collection.mutable.HashMap
 import scala.collection.mutable.LinkedHashMap
 import scala.collection.mutable.ListBuffer
 
 import org.prolog4j._
+
+import java.util.UUID
 
 class ExternalConditionsMap[V](
   override val self : HashMap[String,V]
@@ -62,6 +64,47 @@ object ExternalConditions extends Serializable {
         val ecm = new ExternalConditionsMap[Specimen]( new HashMap[String,Specimen]() )
         _exCndMap = Some( ecm )
         ecm
+      }
+    }
+  }
+
+  def getType[T: TypeTag](obj: T) = typeOf[T]
+
+  implicit def captureSpecimen( content : AnyRef ) : Specimen = {
+    Specimen( getType( content ), None, content )
+  }
+
+  def registerContent( content : AnyRef ) : String = {
+    val uuidStr = UUID.randomUUID.toString
+    exCndMap += ( uuidStr -> content )
+    uuidStr
+  }
+  def registerContent( tag : String, content : AnyRef ) : Unit = {
+    exCndMap += ( tag -> content )
+  }
+  def retrieveContent[T : TypeTag](
+    tag : String,
+    treatCastFailureSilently : Boolean = true
+  ) : Option[T] = {
+    try {      
+      for( specimen <- exCndMap.get( tag ) ) yield {      
+        val t = typeOf[T]
+        if ( specimen.originalType <:< t ) {
+          specimen.contents.asInstanceOf[T]
+        }
+        else {
+          throw new java.lang.ClassCastException()
+        }
+      }
+    }
+    catch {
+      case e : java.lang.ClassCastException => {
+        if ( treatCastFailureSilently ) {
+          None
+        }
+        else {
+          throw e
+        }
       }
     }
   }
