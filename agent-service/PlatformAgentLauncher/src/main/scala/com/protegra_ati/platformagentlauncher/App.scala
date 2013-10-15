116692011f368738159267e2e51681d2621cafbf
renns <raphael.enns@protegra.com>
2013-10-15 11:15:30 -0500

    Removed PlatformAgentLauncher

--- agent-service/PlatformAgentLauncher/src/main/scala/com/protegra_ati/platformagentlauncher/App.scala
+++
@@ -1,9 +0,0 @@
-package com.protegra_ati.platformagentlauncher
-
-import com.biosimilarity.evaluator.distribution.diesel._
-
-object App {
-  def main(args: Array[String]) {
-    Server.run()
-  }
-}
