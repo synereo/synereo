f227e160fe1953488bb06c1ab368bc4614f658e5
Mike Stay <metaweta@gmail.com>
2013-11-26 16:46:53 -0700

    Update match for vUID("pattern")

--- gloseval/src/main/scala/com/biosimilarity/evaluator/spray/EvalHandler.scala
+++ gloseval/src/main/scala/com/biosimilarity/evaluator/spray/EvalHandler.scala
@@ -1020,7 +1020,7 @@ trait EvalHandler {
     ccl match {
       case CnxnCtxtBranch("all", uid :: factuals) => {
         val uidStr = uid match {
-          case CnxnCtxtBranch(_, List(CnxnCtxtBranch(tag,_))) => tag.substring(2)
+          case CnxnCtxtBranch(_, CnxnCtxtLeaf(Left(uidStr: String))) => uidStr
         }
         (uidStr, "all(" + factuals.map("[" + cclToPath(_).reverse.mkString(",") + "]").mkString(",") + ")")
       }
