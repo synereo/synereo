fc7fae34be873db34c34de370906d6e028a60c77
Meredith Gregory <lgreg.meredith@biosimilarity.com>
2014-02-13 17:47:35 -0800

    More preliminaries for the driver

--- agent-service/AgentServices-Store/src/main/scala/com/protegra_ati/agentservices/protocols/verification/Driver.scala
+++ agent-service/AgentServices-Store/src/main/scala/com/protegra_ati/agentservices/protocols/verification/Driver.scala
@@ -21,5 +21,31 @@ package usage {
   import com.biosimilarity.evaluator.distribution.NodeStreams
   import com.biosimilarity.evaluator.distribution.FuzzyStreams
   object VerificationDriver extends NodeStreams with FuzzyStreams {
+    def nextClaimant() : ClaimantBehavior = {
+      ClaimantBehavior()
+    }
+    def nextVerifier() : VerifierBehavior = {
+      VerifierBehavior()
+    }
+    def nextRelyingParty() : RelyingPartyBehavior = {
+      RelyingPartyBehavior()
+    }
+    def claimantStrm() : Stream[ClaimantBehavior] = {
+      tStream( nextClaimant() )( { clmnt => nextClaimant() } )
+    }
+    def verifierStrm() : Stream[VerifierBehavior] = {
+      tStream( nextVerifier() )( { vrfr => nextVerifier() } )
+    }
+    def relyingPartyStrm() : Stream[RelyingPartyBehavior] = {
+      tStream( nextRelyingParty() )( { rp => nextRelyingParty() } )
+    }    
+    def verificationEnsembleStrm(
+    ) : Stream[(ClaimantBehavior,VerifierBehavior,RelyingPartyBehavior)] = {
+      for(
+        ( c, ( v, r ) ) <- claimantStrm().zip( verifierStrm().zip( relyingPartyStrm() ) )
+      ) yield {
+        ( c, v, r )
+      }
+   }
   }
 }
