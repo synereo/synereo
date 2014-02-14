f5b06396b3d42ca372b2526c3cc05768ab728bfb
Meredith Gregory <lgreg.meredith@biosimilarity.com>
2014-02-13 20:27:27 -0800

    Basic shape of the verification protocol driver

--- agent-service/AgentServices-Store/src/main/scala/com/protegra_ati/agentservices/protocols/verification/Driver.scala
+++ agent-service/AgentServices-Store/src/main/scala/com/protegra_ati/agentservices/protocols/verification/Driver.scala
@@ -18,17 +18,60 @@ import scala.util.continuations._
 import java.util.UUID
 
 package usage {  
+  import com.biosimilarity.evaluator.distribution.diesel.DieselEngineScope._
+  import com.biosimilarity.evaluator.distribution.diesel.EvalNodeMapper
+  import com.biosimilarity.evaluator.distribution.diesel.DieselEngineCtor
+  import com.biosimilarity.evaluator.distribution.diesel.DieselEngineCtor.StdEvalChannel
+
   import com.biosimilarity.evaluator.distribution.NodeStreams
   import com.biosimilarity.evaluator.distribution.FuzzyStreams
   import com.biosimilarity.evaluator.distribution.FuzzyTerms
   import com.biosimilarity.evaluator.distribution.FuzzyTermStreams
+
+  import com.protegra_ati.agentservices.store.extensions.StringExtensions._
+
   import com.biosimilarity.lift.model.store.CnxnString
+  import scala.concurrent.FJTaskRunnersX
+
+  trait GLoSStubT
+    extends NodeStreams
+     with FuzzyStreams
+     with FuzzyTerms
+     with FuzzyTermStreams
+     with CnxnString[String,String,String] 
+     with Serializable
+  {
+    def claimantToGLoS : PortableAgentCnxn
+    def verifierToGLoS : PortableAgentCnxn
+    def relyingPartyToGLoS : PortableAgentCnxn
+    def waitForSignalToInitiateClaim(
+      continuation : () => Unit
+    ) : Unit
+  }    
+
+  case class GLoSStub(
+    override val claimantToGLoS : PortableAgentCnxn,
+    override val verifierToGLoS : PortableAgentCnxn,
+    override val relyingPartyToGLoS : PortableAgentCnxn
+  ) extends GLoSStubT {
+    override def waitForSignalToInitiateClaim(
+      continuation : () => Unit
+    ) : Unit = {     
+      println( "Please state your claim: " )
+      val ln = readLine() // Note: this is blocking.
+      val claim = ln.toLabel
+      continuation()
+      println( "your claim, " + claim + ", has been submitted" )
+     }
+  }
+
   object VerificationDriver
    extends NodeStreams
      with FuzzyStreams
      with FuzzyTerms
      with FuzzyTermStreams
      with CnxnString[String,String,String] 
+     with FJTaskRunnersX
      with Serializable
   {
     def nextClaimant() : ClaimantBehavior = {
@@ -82,6 +125,56 @@ package usage {
       ) yield {
         ( c, v, r )
       }
-   }
+    }
+
+    def verificationEnsembleTestStrm(
+      node : StdEvalChannel,
+      glosStub : GLoSStub,
+      clmntBhvr : ClaimantBehavior,
+      vrfrBhvr : VerifierBehavior,
+      rpBhvr : RelyingPartyBehavior
+    )(
+      cnxnStrm : Stream[(PortableAgentCnxn,PortableAgentCnxn,PortableAgentCnxn,PortableAgentCnxn,PortableAgentCnxn,PortableAgentCnxn)]
+    ): Stream[() => Unit] = {      
+      for(
+        ( c, v, r, c2v, c2r, v2r ) <- cnxnStrm
+      ) yield {
+        () => {
+          glosStub.waitForSignalToInitiateClaim(
+            () => {
+              spawn { 
+                clmntBhvr.run(
+                  node,
+                  List[PortableAgentCnxn](
+                    glosStub.claimantToGLoS
+                  ),
+                  List[CnxnCtxtLabel[String, String, String]]( )
+                )
+              }
+              spawn {
+                vrfrBhvr.run(
+                  node,
+                  List[PortableAgentCnxn](
+                    glosStub.verifierToGLoS,
+                    c2v
+                  ),
+                  List[CnxnCtxtLabel[String, String, String]]( )
+                )
+              }
+              spawn {
+                rpBhvr.run(
+                  node,
+                  List[PortableAgentCnxn](
+                    glosStub.relyingPartyToGLoS,
+                    c2r
+                  ),
+                  List[CnxnCtxtLabel[String, String, String]]( )
+                )
+              }
+            }
+          )
+        }                
+      }
+    }
   }
 }
