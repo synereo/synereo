95b60ee35f634dc0afcaf7794c8ece3477ecb591
Meredith Gregory <lgreg.meredith@biosimilarity.com>
2014-03-10 10:59:56 -0700

    For accordion configuration

---
+++ agent-service/AgentServices-Store/src/main/scala/com/protegra_ati/agentservices/store/Accordion.scala
@@ -0,0 +1,80 @@
+// -*- mode: Scala;-*- 
+// Filename:    Accordion.scala 
+// Authors:     lgm                                                    
+// Creation:    Sun Mar  9 18:30:57 2014 
+// Copyright:   Not supplied 
+// Description: 
+// ------------------------------------------------------------------------
+
+package com.biosimilarity.evaluator.distribution
+
+import com.biosimilarity.lift.model.ApplicationDefaults
+import com.biosimilarity.lift.model.store._
+import com.biosimilarity.lift.model.store.xml._
+//import com.biosimilarity.lift.model.store.mongo._
+import com.biosimilarity.lift.model.agent._
+import com.biosimilarity.lift.model.msg._
+import com.biosimilarity.lift.lib._
+import com.biosimilarity.lift.lib.moniker._
+import net.liftweb.amqp._
+
+import scala.util.continuations._ 
+import scala.concurrent.{Channel => Chan, _}
+//import scala.concurrent.cpsops._
+import scala.xml._
+import scala.collection.mutable.Map
+import scala.collection.mutable.MapProxy
+import scala.collection.mutable.HashMap
+import scala.collection.mutable.LinkedHashMap
+import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.Stack
+import scala.collection.mutable.MutableList
+
+//import com.rabbitmq.client._
+
+import org.prolog4j._
+
+import com.mongodb.casbah.Imports._
+
+//import org.json4s._
+//import org.json4s.jackson.JsonMethods._
+//import org.json4s.jackson.Serialization
+import org.json4s.jackson.Serialization.{read, write}
+
+import com.thoughtworks.xstream.XStream
+import com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver
+
+import biz.source_code.base64Coder.Base64Coder
+
+import javax.xml.transform.OutputKeys
+
+import java.util.UUID
+import java.net.URI
+import java.util.Properties
+import java.io.File
+import java.io.FileInputStream
+import java.io.OutputStreamWriter
+import java.io.ObjectInputStream
+import java.io.ByteArrayInputStream
+import java.io.ObjectOutputStream
+import java.io.ByteArrayOutputStream
+
+trait DeploymentMode
+case object Colocated extends DeploymentMode
+case object Distributed extends DeploymentMode
+
+trait AccordionConfiguration {
+  self : EvalConfig =>
+    def deploymentMode() : DeploymentMode = {
+      try {
+        val dm = evalConfig().getString( "deploymentMode" )
+        dm match {
+          case "colocated" => Colocated
+          case "distributed" => Distributed
+        }
+      }
+      catch {
+        case e : Throwable => Distributed
+      }
+    }
+}
