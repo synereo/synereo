af6d6e1772c2db428ab0aad2ee7774d9ace1580f
Meredith Gregory <lgreg.meredith@biosimilarity.com>
2014-03-18 15:16:33 -0700

    Changed the node user

--- gloseval/src/main/scala/com/biosimilarity/evaluator/spray/NodeUser.scala
+++ gloseval/src/main/scala/com/biosimilarity/evaluator/spray/NodeUser.scala
@@ -1,7 +1,7 @@
 package com.biosimilarity.evaluator.spray
 
 object NodeUser {
-  val email = "individualagenttechnology@googlegroups.com"
+  val email = "splicious-design-team@googlegroups.com"
   val password = "U32vbN6WVJ"
-  val jsonBlob = "{\"name\":\"ATI\",\"imgSrc\":\"\"}"
+  val jsonBlob = "{\"name\":\"Queen Splicious\",\"imgSrc\":\"\"}"
 }
