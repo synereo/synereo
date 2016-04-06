dbae2fec810ee9b5d214396edd978389ad2f7c5f
n <ns68751@gmail.com>
2016-04-05 19:43:16 -0700

    Added prepare-package and ip address

--- gloseval/src/main/scala/com/biosimilarity/evaluator/spray/Boot.scala
+++ gloseval/src/main/scala/com/biosimilarity/evaluator/spray/Boot.scala
@@ -33,6 +33,6 @@ object Boot extends App
    //newHttpServer(service) ! Bind(interface = "0.0.0.0", port = 9876)
 
 
-   IO(Http) ! Http.Bind(listener = service, interface = "127.0.0.1", port = 9876)
+   IO(Http) ! Http.Bind(listener = service, interface = "0.0.0.0", port = 9876)
 
 }
