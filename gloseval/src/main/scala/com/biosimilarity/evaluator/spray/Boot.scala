7889b0bf9bd185e8d6d8d8213026c6aeb707c224
n10n <ns68751@gmail.com>
2016-05-23 18:23:26 -0700

    Reverted to prior version after importer merge

--- gloseval/src/main/scala/com/biosimilarity/evaluator/spray/Boot.scala
+++ gloseval/src/main/scala/com/biosimilarity/evaluator/spray/Boot.scala
@@ -4,23 +4,20 @@ import akka.io.IO
 import spray.can.Http
 import akka.actor.{ActorSystem, Props}
 
-
 object Boot extends App
- with Serializable {
+  with Serializable {
 
   //TODO: Remove sleep below once race condition is fixed
   // @@GS - is it fixed??
   Thread.sleep(3000)
 
-   com.biosimilarity.evaluator.distribution.bfactory.BFactoryMapInitializer.makeMap()
+  com.biosimilarity.evaluator.distribution.bfactory.BFactoryMapInitializer.makeMap()
 
-   @transient
-   implicit val system = ActorSystem("evaluator-system")
+  @transient
+  implicit val system = ActorSystem("evaluator-system")
 
-   @transient
-   val service = system.actorOf(Props[EvaluatorServiceActor], "evaluator-service")   
+  @transient
+  val service = system.actorOf(Props[EvaluatorServiceActor], "evaluator-service")   
 
-   //IO(Http) ! Http.Bind(listener = service, interface = "0.0.0.0", port = 9876)
-   IO(Http) ! Http.Bind(listener = service, interface = "127.0.0.1", port = 9876)
-
+  IO(Http) ! Http.Bind(listener = service, interface = "0.0.0.0", port = 9876)
 }
