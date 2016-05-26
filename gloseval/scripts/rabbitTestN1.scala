1b0d22e7cd3444e0ceece52950019a7970897096
n10n <ns68751@gmail.com>
2016-05-26 13:35:50 -0700

    Update and rename rabbitTest.scala to rabbitTestN1.scala

--- gloseval/scripts/rabbitTestN1.scala
+++ gloseval/scripts/rabbitTestN1.scala
@@ -8,8 +8,11 @@ import java.net.URI
 import com.biosimilarity.lift.lib._ 
 import com.biosimilarity.lift.lib.usage.AMQPTPSample._ 
 import _root_.com.rabbitmq.client.{Channel=>RabbitChan, _} 
-                                                                           
-val sourceHost1 = new URI( "amqp://guest:guest@192.168.99.100:5672/synereo" )  
-val targetHost1 = new URI( "amqp://guest:guest@192.168.99.100:5672/synereo" ) 
-                                                                 
+
+val sIP="192.168.99.100"   
+val tIP="192.168.99.100"
+val sourceHost1 = new URI(s"amqp://guest:guest@$sIP:5672/synereo" )  
+val targetHost1 = new URI(s"amqp://guest:guest@$tIP:5672/synereo" ) 
+
+//For first node                                                                 
 setupAndRunTest( true, sourceHost1, targetHost1, "synereo1", true, 10)
