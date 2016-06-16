e9af92c74d77b21754bdb6e26ce2d1a4b5464cc0
Henry Till <henrytill@gmail.com>
2016-06-15 21:28:24 -0400

    SOC-158 project/Dependencies.scala: update coreDeps
    Many dependencies from the pom.xml appear to be currently unused and
    have no effect on the build.

--- specialk/project/Dependencies.scala
+++ specialk/project/Dependencies.scala
@@ -15,38 +15,47 @@ object Dependencies {
   lazy val xmlrpcVersion = "3.1.2"
 
   lazy val coreDeps = Seq(
+    // =================================================
+    // These dependencies appear to be currently unused:
+    // =================================================
+    // "com.google.zxing"          % "core"              % "2.0",
+    // "com.h2database"            % "h2"                % "1.2.138",
+    // "javax.xml.xquery"          % "xqj-api"           % "1.0",
+    // "junit"                     % "junit"             % "4.7"     % "test",
+    // "net.lag"                   % "configgy"          % "???",
+    // "org.apache.xmlrpc"         % "xmlrpc-client"     % xmlrpcVersion exclude("javax.servlet", "servlet-api"),
+    // "org.apache.xmlrpc"         % "xmlrpc-common"     % xmlrpcVersion exclude("javax.servlet", "servlet-api"),
+    // "org.apache.xmlrpc"         % "xmlrpc-server"     % xmlrpcVersion exclude("javax.servlet", "servlet-api"),
+    // "org.eclipse.jetty"         % "jetty-http"        % jettyVersion,
+    // "org.eclipse.jetty"         % "jetty-server"      % jettyVersion exclude("org.mortbay.jetty", "servlet-api"),
+    // "org.eclipse.jetty"         % "jetty-servlets"    % jettyVersion,
+    // "org.eclipse.jetty"         % "jetty-webapp"      % jettyVersion,
+    // "org.eclipse.jetty"         % "jetty-websocket"   % jettyVersion,
+    // "org.eclipse.jetty.orbit"   % "javax.servlet.jsp" % "2.2.0.v201112011158",
+    // "org.scalatest"            %% "scalatest"         % "2.0.M5b" % "test",
+    // "org.specs2"               %% "specs2"            % "1.14"    % "test",
+    // "xpp3"                      % "xpp3_min"          % "1.1.4c",
+    // ===============================================================
+    // These dependencies are currently unmanaged (see lib directory):
+    // ===============================================================
     // "org.prolog4j"              % "prolog4j-api"      % prolog4jVersion,
     // "org.prolog4j"              % "prolog4j-tuprolog" % prolog4jVersion,
-    "it.unibo.alice.tuprolog"   % "tuprolog"          % "2.1.1",
-    "com.google.zxing"          % "core"              % "2.0",
-    "org.mongodb"              %% "casbah"            % "2.6.4",
-    "org.json4s"               %% "json4s-jackson"    % "3.2.7",
+    // =====================
+    // Current dependencies:
+    // =====================
     "biz.source_code"           % "base64coder"       % "2010-09-21",
-    "org.coconut.forkjoin"      % "jsr166y"           % "070108",
-    "com.h2database"            % "h2"                % "1.2.138",
-    "junit"                     % "junit"             % "4.7"        % "test",
-    "org.specs2"               %% "specs2"            % "1.14"       % "test",
-    "org.scalatest"            %% "scalatest"         % "2.0.M5b"    % "test",
-    "org.eclipse.jetty.orbit"   % "javax.servlet.jsp" % "2.2.0.v201112011158",
-    "org.eclipse.jetty"         % "jetty-server"      % jettyVersion,
-    "org.eclipse.jetty"         % "jetty-http"        % jettyVersion,
-    "org.eclipse.jetty"         % "jetty-servlets"    % jettyVersion,
-    "org.eclipse.jetty"         % "jetty-webapp"      % jettyVersion,
-    "org.eclipse.jetty"         % "jetty-websocket"   % jettyVersion,
-    "com.typesafe"              % "config"            % "1.0.0",
-    // "net.lag"                   % "configgy"          % "???",
     "com.rabbitmq"              % "amqp-client"       % "2.6.1",
-    "org.codehaus.jettison"     % "jettison"          % "1.3",
-    "xpp3"                      % "xpp3_min"          % "1.1.4c",
-    "com.thoughtworks.xstream"  % "xstream"           % "1.4.4",
-    "javax.persistence"         % "persistence-api"   % "1.0",
+    "com.thoughtworks.xstream"  % "xstream"           % "1.4.4" exclude("xmlpull", "xmlpull"),
+    "com.typesafe"              % "config"            % "1.0.0",
     "commons-pool"              % "commons-pool"      % "1.6",
-    "org.apache.xmlrpc"         % "xmlrpc-client"     % xmlrpcVersion,
-    "org.apache.xmlrpc"         % "xmlrpc-common"     % xmlrpcVersion,
-    "org.apache.xmlrpc"         % "xmlrpc-server"     % xmlrpcVersion,
-    "javax.xml.xquery"          % "xqj-api"           % "1.0",
-    "org.basex"                 % "basex-api"         % "7.6",
+    "it.unibo.alice.tuprolog"   % "tuprolog"          % "2.1.1",
+    "javax.persistence"         % "persistence-api"   % "1.0",
     "log4j"                     % "log4j"             % "1.2.17",
+    "org.basex"                 % "basex-api"         % "7.6",
+    "org.coconut.forkjoin"      % "jsr166y"           % "070108",
+    "org.codehaus.jettison"     % "jettison"          % "1.3",
+    "org.json4s"               %% "json4s-jackson"    % "3.2.7",
+    "org.mongodb"              %% "casbah"            % "2.6.4",
     "org.scalesxml"            %% "scales-xml"        % "0.4.5")
 
   lazy val depsSettings = Seq(
