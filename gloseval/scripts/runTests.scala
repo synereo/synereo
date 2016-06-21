be9902ad4470fc614469dd6530e6d2377b07d9d1
Gary Stephenson <gary@oxide.net.au>
2016-06-22 08:17:49 +1000

    omnilayer continued

--- gloseval/scripts/runTests.scala
+++ gloseval/scripts/runTests.scala
@@ -1,5 +1,9 @@
-import com.biosimilarity.evaluator.importer.Importer
-import com.biosimilarity.evaluator.omniRPC.OmniClient
+//import com.protegra_ati.agentservices.protocols.TestRace
+//TestRace.race(true, 20, false, 2)
 
+
+//import com.biosimilarity.evaluator.importer.Importer
 //Importer.runTestFiles()
+
+import com.biosimilarity.evaluator.omniRPC.OmniClient
 OmniClient.runTests()
