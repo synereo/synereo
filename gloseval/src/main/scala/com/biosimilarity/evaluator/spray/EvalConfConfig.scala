f7753985ab2ab4225397b35e2da52084813c1cb3
Henry Till <henrytill@gmail.com>
2016-08-05 14:13:10 -0400

    EvalConfConfig: clean up

--- gloseval/src/main/scala/com/biosimilarity/evaluator/spray/EvalConfConfig.scala
+++ gloseval/src/main/scala/com/biosimilarity/evaluator/spray/EvalConfConfig.scala
@@ -1,34 +1,40 @@
 package com.biosimilarity.evaluator.distribution
 
+import com.typesafe.config.Config
+
+import scala.util.Try
+
 object EvalConfConfig extends EvalConfig {
 
-  val config = evalConfig()
+  val config: Config = evalConfig()
 
-  def read(prm : String ) : String = {
-    try { config.getString(prm) }
-    catch { case _ : Throwable => throw new Exception("Missing or empty value for: " +prm + " in eval.conf file.") }
-  }
+  def read(param: String): String =
+    try {
+      config.getString(param)
+    } catch {
+      case _: Throwable => throw new IllegalStateException(s"Missing or empty value for: $param in eval.conf file.")
+    }
 
-  def read(prm : String, dflt : String ) : String = {
-    try { config.getString(prm) }
-    catch { case _ : Throwable => dflt }
-  }
+  def read(param: String, default: String): String =
+    try {
+      config.getString(param)
+    } catch {
+      case _: Throwable => default
+    }
 
-  def readInt(prm : String ) : Int = {
-    try { config.getInt(prm) }
-    catch { case _ : Throwable => throw new Exception("Missing or empty value for: " +prm + " in eval.conf file.") }
-  }
+  def readInt(param: String): Int =
+    try {
+      config.getInt(param)
+    } catch {
+      case _: Throwable => throw new IllegalStateException(s"Missing or empty value for: $param in eval.conf file.")
+    }
 
-  def readInt(prm : String, dflt : Int ) : Int = {
-    try { config.getInt(prm) }
-    catch { case _ : Throwable => dflt }
-  }
+  def readInt(param: String, default: Int): Int =
+    try {
+      config.getInt(param)
+    } catch {
+      case _: Throwable => default
+    }
 
-  def isOmniRequired() : Boolean = {
-    read("OmniRPCURI", "miss") != "miss"
-  }
-
+  def isOmniRequired: Boolean = Try(read("OmniRPCURI")).isSuccess
 }
-
-
-
