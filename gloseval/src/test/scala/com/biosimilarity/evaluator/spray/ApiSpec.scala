a2f04c74b8183670b859d0f0e20ee7d9c786cabc
Gary Stephenson <gary@oxide.net.au>
2016-08-17 21:19:57 +1000

    add api test for query empty database

--- gloseval/src/test/scala/com/biosimilarity/evaluator/spray/ApiSpec.scala
+++ gloseval/src/test/scala/com/biosimilarity/evaluator/spray/ApiSpec.scala
@@ -9,6 +9,7 @@ import akka.util.Timeout
 import com.biosimilarity.evaluator.Api
 import com.biosimilarity.evaluator.distribution.EvalConfConfig._
 import com.biosimilarity.evaluator.spray.ClientSSLConfiguration._
+import com.biosimilarity.evaluator.spray.CapUtilities
 import com.typesafe.config.{Config, ConfigFactory}
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
@@ -25,7 +26,7 @@ import org.scalatest.time.{Seconds, Span}
 import scala.concurrent.Future
 import scala.concurrent.duration._
 
-class ApiSpec extends WordSpec with Matchers with BeforeAndAfterAll with ScalaFutures with IntegrationPatience {
+class ApiSpec extends WordSpec with Matchers with BeforeAndAfterAll with ScalaFutures with IntegrationPatience with CapUtilities {
 
   val config: Config = ConfigFactory.load()
   val settings: ServerSettings = ServerSettings(config)
@@ -66,15 +67,11 @@ class ApiSpec extends WordSpec with Matchers with BeforeAndAfterAll with ScalaFu
 
   type SessionUri = String
 
-  def post(req: HttpRequest): Future[HttpResponse] = {
+  def post(cont: Api.RequestContent): Future[HttpResponse] = {
+    val req = makeRequest(cont)
     eventualHostConnector
       .flatMap((hc: ActorRef) => hc.ask(req)(timeout))
       .mapTo[HttpResponse]
-  }
-
-  def post(cont: Api.RequestContent): Future[HttpResponse] = {
-    val req = makeRequest(cont)
-    post(req)
   }
 
   def openSession(username: String, password: String): Future[SessionUri] = {
@@ -111,11 +108,16 @@ class ApiSpec extends WordSpec with Matchers with BeforeAndAfterAll with ScalaFu
     })
   }
 
+  def capFromAgentUri(agent: AgentUri) = {
+    agent.replace("agent://cap/", "").slice(0, 36)
+  }
+
+  def makeAliasUri(agent: AgentUri) = {
+    val cap = capFromAgentUri(agent)
+    s"alias://$cap/alias"
+  }
+
   def makeConnection(sessionId: SessionUri, agentL: AgentUri, agentR: AgentUri, cnxnLabel: String): Future[Api.Connection] = {
-    def makeAliasUri(agent: String) = {
-        val cap = agent.replace("agent://cap/", "").slice(0, 36)
-        s"alias://$cap/alias"
-    }
     val sourceUri = makeAliasUri(agentL)
     val targetUri = makeAliasUri(agentR)
     //val cnxnLabel = UUID.randomUUID().toString
@@ -124,6 +126,30 @@ class ApiSpec extends WordSpec with Matchers with BeforeAndAfterAll with ScalaFu
     post(cont).map((response: HttpResponse) => {
       //val rsp = response.entity.asString
       Api.Connection(sourceUri, targetUri, cnxnLabel)
+    })
+  }
+
+  def makePost(sessionId: SessionUri, tgts: List[Api.Connection], lbl: String, uid: String, value: String): Future[Api.Connection] = {
+    val from = "agent://" + capFromSession(sessionId)
+    val selfcnxn = Api.Connection(from, from, "alias")
+
+    val cont = Api.EvalSubscribeContent(selfcnxn :: tgts, lbl, value, uid)
+    val req = Api.EvalSubscribeRequest(sessionId, Api.EvalSubscribeExpression("insertContent", cont))
+    post(req).map((response: HttpResponse) => {
+      //val rsp = response.entity.asString
+      selfcnxn
+    })
+  }
+
+  def makeQueryOnSelf(sessionId: SessionUri, lbl: String ): Future[Api.Connection] = {
+    val from = "agent://" + capFromSession(sessionId)
+    val selfcnxn = Api.Connection(from, from, "alias")
+
+    val cont = Api.EvalSubscribeContent(selfcnxn :: Nil, lbl, "", "")
+    val req = Api.EvalSubscribeRequest(sessionId, Api.EvalSubscribeExpression("feedExpr", cont))
+    post(req).map((response: HttpResponse) => {
+      //val rsp = response.entity.asString
+      selfcnxn
     })
   }
 
@@ -142,7 +168,26 @@ class ApiSpec extends WordSpec with Matchers with BeforeAndAfterAll with ScalaFu
     })
   }
 
+  /*
+  def pingUntilPong(ssn: SessionUri): Future[JArray] = {
+    val cont = Api.SessionPing(ssn)
+    def _pingRec(cur: List[JValue]) : Future[List[JValue]] = {
+      post(cont).map((response: HttpResponse) => {
+        val ja = parse(response.entity.asString).extract[JArray]
+        var done = false
+        var rslt = cur
+        ja.values.foreach[JValue]( (jv : JValue) => {
+          val typ = (jv \ "msgType").extract[String]
+          if (typ == "sessionPong") done = true
+          else rslt = jv :: cur
+        })
+      })
+    }
+  }
+  */
+
   """The Administrator Session""".stripMargin should {
+
     """establish the correct number of connections""" in {
       val proc: Future[(JArray, JArray, JArray)] = for {
         ssn <- openAdminSession()
@@ -172,12 +217,32 @@ class ApiSpec extends WordSpec with Matchers with BeforeAndAfterAll with ScalaFu
 
       whenReady(proc, timeout(Span(60, Seconds))) {
         case (ja: JArray, jb: JArray, jc: JArray) =>
-          println("Alice's connections: "+pretty(render(ja)))
-          println("Bob's connections: "+pretty(render(jb)))
-          println("Carol's connections: "+pretty(render(jc)))
+          //println("Alice's connections: "+pretty(render(ja)))
+          //println("Bob's connections: "+pretty(render(jb)))
+          //println("Carol's connections: "+pretty(render(jc)))
           ja.values.length shouldBe 3
           jb.values.length shouldBe 2
           jc.values.length shouldBe 2
+        case _ => fail("should not happen")
+      }
+    }
+
+    """query empty database without crashing""" in {
+//      val value = "{\"$type\":\"shared.models.MessagePost\", \"created\" : \"2002-05-30T09:30:10Z\", \"modified\" : \"2002-05-30T09:30:10Z\", \"messagePostContent\": {\"$type\":\"shared.models.MessagePostContent\",\"versionedPostId\" : \"35e60447-747e-496a-afde-65ca182db1c8\", \"versionedPostPredecessorId\" : \"\", \"versionNumber\" : \"1\", \"canForward\" : true, \"text\" : \"this is a subject for the message\",\"subject\" : \"this is a message\"}}"
+//      val uid = "0c778b40-4799-4557-9050-fd7a4b77c23e"
+
+      val proc: Future[(JArray)] = for {
+        ssn <- openAdminSession()
+        cnxn <- makeQueryOnSelf(ssn, "each([MESSAGEPOSTLABEL])")
+        spwnssnA <- spawnSession(ssn)
+        a <- sessionPing(spwnssnA)
+
+      } yield (a)
+
+      whenReady(proc, timeout(Span(60, Seconds))) {
+        case (ja: JArray) =>
+          println("Query results: "+pretty(render(ja)))
+          ja.values.length shouldBe 1
         case _ => fail("should not happen")
       }
     }
