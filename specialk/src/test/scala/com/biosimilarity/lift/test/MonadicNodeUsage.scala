05835f7fe12b22223b0018b3fa7f859c6142878e
Henry Till <henrytill@gmail.com>
2016-09-08 01:55:51 -0400

    specialk: update MonadicNodeUsage

--- specialk/src/test/scala/com/biosimilarity/lift/test/MonadicNodeUsage.scala
+++ specialk/src/test/scala/com/biosimilarity/lift/test/MonadicNodeUsage.scala
@@ -5,7 +5,7 @@ import com.biosimilarity.lift.model.store.MonadicJSONFramedMsgDispatcher
 /**
   * lifted from MonadicNode.scala
   */
-class MonadicNodeUsage {
+object MonadicNodeUsage {
 
   import java.net.URI
 
@@ -46,20 +46,19 @@ class MonadicNodeUsage {
 
     type FUCPD = FramedUseCaseProtocolDispatcher
 
+    type OM = Option[MsgStreamFactory.Message]
+
     implicit val retTwist: Boolean = false
 
     def setup(localHost: String, localPort: Int, remoteHost: String, remotePort: Int)(
         implicit returnTwist: Boolean): Either[FUCPD, (FUCPD, FUCPD)] = {
-
       val (localExchange, remoteExchange) = if (localHost.equals(remoteHost) && (localPort == remotePort)) {
         ("/useCaseProtocolLocal", "/useCaseProtocolRemote")
       } else {
         ("/useCaseProtocol", "/useCaseProtocol")
       }
-
       val localURI  = new URI("agent", null, localHost, localPort, localExchange, null, null)
       val remoteURI = new URI("agent", null, remoteHost, remotePort, remoteExchange, null, null)
-
       if (returnTwist)
         Right[FUCPD, (FUCPD, FUCPD)](
           (FramedUseCaseProtocolDispatcher(localURI, remoteURI), FramedUseCaseProtocolDispatcher(remoteURI, localURI)))
@@ -69,85 +68,140 @@ class MonadicNodeUsage {
 
     implicit val numberOfMsgs: Int = 100
 
-    def runClient(dispatcher: FUCPD)(implicit numMsgs: Int): Unit = {
-
-      type OM = Option[MsgStreamFactory.Message]
-
-      val reqs = MsgStreamFactory
-        .msgStream[UseCaseRequest] { (b: Boolean, i: Int, a: String, r: OM) =>
-          UseCaseRequestOne(b, i, a, r)
-        }
+    def runClient(ds: Either[FUCPD, (FUCPD, FUCPD)])(implicit numMsgs: Int): Unit = {
+      val reqs: List[FramedMsgDispatcherUseCase.UseCaseRequest] = MsgStreamFactory
+        .msgStream[UseCaseRequest]((b: Boolean, i: Int, a: String, r: OM) => UseCaseRequestOne(b, i, a, r))
         .take(numMsgs)
         .toList
-
-      // map-reduce-style protocol checking
-
       val msgMap = new mutable.HashMap[Int, Either[UseCaseRequest, (UseCaseRequest, UseCaseResponse)]]()
-
-      msgMap += (0 -> Left[UseCaseRequest, (UseCaseRequest, UseCaseResponse)](reqs(0)))
-      dispatcher ! reqs(0)
-
-      val thr: Thread = new Thread {
-        override def run(): Unit = reset {
-          for (msg <- dispatcher ? ()) {
-            BasicLogService.tweet(dispatcher + " received: " + msg)
-            msg match {
-              case Right(rsp @ UseCaseResponseOne(b, j, a, r)) =>
-                BasicLogService.tweet(dispatcher + " handling response for the " + j + "th " + "request")
-                msgMap.get(j) match {
-                  case Some(Left(req)) =>
-                    msgMap += (j -> Right[UseCaseRequest, (UseCaseRequest, UseCaseResponse)]((req, rsp)))
-                    if (j < (numMsgs - 1)) {
-                      // Open with left brace ...
-                      msgMap += ((j + 1) -> Left[UseCaseRequest, (UseCaseRequest, UseCaseResponse)](reqs(j + 1)))
-                      dispatcher ! reqs(j + 1)
-                    } else
-                      BasicLogService.tweet("Client side of test complete.")
-                  case unExpected @ _ =>
-                    throw new Exception("Protocol violated. Received: " + unExpected)
+      msgMap += (0 -> Left[UseCaseRequest, (UseCaseRequest, UseCaseResponse)](reqs.head))
+      ds match {
+        case Left(dispatcher) =>
+          dispatcher ! reqs.head
+          val thread: Thread = new Thread {
+            override def run(): Unit =
+              reset {
+                for (msg <- dispatcher ? ()) {
+                  BasicLogService.tweet(s"$dispatcher received: $msg")
+                  msg match {
+                    case Right(rsp @ UseCaseResponseOne(b, j, a, r)) =>
+                      BasicLogService.tweet(s"$dispatcher handling response for the ${j}th request")
+                      msgMap.get(j) match {
+                        case Some(Left(req)) =>
+                          msgMap += (j -> Right[UseCaseRequest, (UseCaseRequest, UseCaseResponse)]((req, rsp)))
+                          if (j < (numMsgs - 1)) {
+                            msgMap += ((j + 1) -> Left[UseCaseRequest, (UseCaseRequest, UseCaseResponse)](reqs(j + 1)))
+                            dispatcher ! reqs(j + 1)
+                          } else
+                            BasicLogService.tweet("Client side of test complete.")
+                        case unExpected @ _ =>
+                          throw new Exception(s"Protocol violated. Received: $unExpected")
+                      }
+                    case unExpected @ _ =>
+                      throw new Exception(s"Protocol violated. Received: $unExpected")
+                  }
                 }
-              case unExpected @ _ =>
-                throw new Exception("Protocol violated. Received: " + unExpected)
-            }
+              }
           }
-        }
+          thread.start()
+        case Right((dispatcherSend, dispatcherReceive)) =>
+          dispatcherSend ! reqs.head
+          val thread: Thread = new Thread {
+            override def run(): Unit =
+              reset {
+                for (msg <- dispatcherReceive ? ()) {
+                  BasicLogService.tweet(s"$dispatcherReceive received: $msg")
+                  msg match {
+                    case Right(rsp @ UseCaseResponseOne(b, j, a, r)) =>
+                      BasicLogService.tweet(s"$dispatcherReceive handling response for the ${j}th request")
+                      msgMap.get(j) match {
+                        case Some(Left(req)) =>
+                          msgMap += (j -> Right[UseCaseRequest, (UseCaseRequest, UseCaseResponse)]((req, rsp)))
+                          if (j < (numMsgs - 1)) {
+                            msgMap += ((j + 1) -> Left[UseCaseRequest, (UseCaseRequest, UseCaseResponse)](reqs(j + 1)))
+                            dispatcherSend ! reqs(j + 1)
+                          } else
+                            BasicLogService.tweet("Client side of test complete.")
+                        case unExpected @ _ =>
+                          throw new Exception(s"Protocol violated. Received: $unExpected")
+                      }
+                    case unExpected @ _ =>
+                      throw new Exception(s"Protocol violated. Received: $unExpected")
+                  }
+                }
+              }
+          }
+          thread.start()
       }
-
-      thr.start()
     }
 
-    def runServer(dispatcher: FUCPD)(implicit numMsgs: Int): Unit = {
-      // map-reduce-style protocol checking
+    def runServer(ds: Either[FUCPD, (FUCPD, FUCPD)])(implicit numMsgs: Int): Unit = {
       val msgMap = new mutable.HashMap[Int, Either[UseCaseRequest, (UseCaseRequest, UseCaseResponse)]]()
-      val thr: Thread = new Thread {
-        override def run(): Unit = reset {
-          for (msg <- dispatcher ? ()) {
-            BasicLogService.tweet(dispatcher + " received: " + msg)
-            msg match {
-              case Left(req @ UseCaseRequestOne(b, j, a, r)) =>
-                BasicLogService.tweet(dispatcher + " handling the " + j + "th " + "request")
-                if (j < numMsgs) {
-                  msgMap.get(j) match {
-                    case None =>
-                      val rsp = UseCaseResponseOne(b, j, a, Some(req))
-                      msgMap +=
-                        (j -> Right[UseCaseRequest, (UseCaseRequest, UseCaseResponse)](req, rsp))
-                      dispatcher !! rsp
-                      if (msgMap.size == numMsgs) {
+      ds match {
+        case Left(dispatcher) =>
+          val thread: Thread = new Thread {
+            override def run(): Unit =
+              reset {
+                for (msg <- dispatcher ? ()) {
+                  BasicLogService.tweet(s"$dispatcher received: $msg")
+                  msg match {
+                    case Left(req @ UseCaseRequestOne(b, j, a, r)) =>
+                      BasicLogService.tweet(s"$dispatcher handling the ${j}th request")
+                      if (j < numMsgs) {
+                        msgMap.get(j) match {
+                          case None =>
+                            val rsp = UseCaseResponseOne(b, j, a, Some(req))
+                            msgMap +=
+                              (j -> Right[UseCaseRequest, (UseCaseRequest, UseCaseResponse)](req, rsp))
+                            dispatcher !! rsp
+                            if (msgMap.size == numMsgs) {
+                              BasicLogService.tweet("Server side of test complete.")
+                            }
+                          case unExpected @ _ =>
+                            throw new Exception(s"Protocol violated. Received: $unExpected")
+                        }
+                      } else
                         BasicLogService.tweet("Server side of test complete.")
-                      }
+                    case unExpected @ _ =>
+                      throw new Exception(s"Protocol violated. Received: $unExpected")
+                  }
+                }
+              }
+          }
+          thread.start()
+        // Flipping the sender and receiver here?
+        case Right((dispatcherReceive, dispatcherSend)) =>
+          val thread: Thread = new Thread {
+            override def run(): Unit =
+              reset {
+                for (msg <- dispatcherReceive ? ()) {
+                  BasicLogService.tweet(dispatcherReceive + " received: " + msg)
+                  msg match {
+                    case Left(req @ UseCaseRequestOne(b, j, a, r)) =>
+                      BasicLogService.tweet(dispatcherReceive + " handling the " + j + "th " + "request")
+                      if (j < numMsgs) {
+                        msgMap.get(j) match {
+                          case None =>
+                            val rsp = UseCaseResponseOne(b, j, a, Some(req))
+                            msgMap +=
+                              (j -> Right[UseCaseRequest, (UseCaseRequest, UseCaseResponse)](req, rsp))
+                            dispatcherSend !! rsp
+                            if (msgMap.size == numMsgs) {
+                              BasicLogService.tweet("Server side of test complete.")
+                            }
+                          case unExpected @ _ =>
+                            throw new Exception("Protocol violated. Received: " + unExpected)
+                        }
+                      } else
+                        BasicLogService.tweet("Server side of test complete.")
                     case unExpected @ _ =>
                       throw new Exception("Protocol violated. Received: " + unExpected)
                   }
-                } else
-                  BasicLogService.tweet("Server side of test complete.")
-              case unExpected @ _ =>
-                throw new Exception("Protocol violated. Received: " + unExpected)
-            }
+                }
+              }
           }
-        }
+          thread.start()
       }
-      thr.start()
     }
   }
 }
