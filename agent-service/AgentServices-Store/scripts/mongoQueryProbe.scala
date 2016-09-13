b7471d38887105884e14bfe9f472cf5ec4f4e6d7
Henry Till <henrytill@gmail.com>
2016-09-13 18:01:00 -0400

    agent-service: update scripts/mongoQueryProbe.scala

--- agent-service/AgentServices-Store/scripts/mongoQueryProbe.scala
+++ agent-service/AgentServices-Store/scripts/mongoQueryProbe.scala
@@ -1,38 +1,38 @@
-// -*- mode: Scala;-*- 
-// Filename:    mongoQueryProbe.scala 
-// Authors:     lgm                                                    
-// Creation:    Tue Aug  6 14:24:40 2013 
-// Copyright:   Not supplied 
-// Description: 
+// -*- mode: Scala;-*-
+// Filename:    mongoQueryProbe.scala
+// Authors:     lgm
+// Creation:    Tue Aug  6 14:24:40 2013
+// Copyright:   Not supplied
+// Description:
 // ------------------------------------------------------------------------
 
 object MongoQueryProbe {
+
+  import com.biosimilarity.evaluator.distribution._
   import com.biosimilarity.evaluator.distribution.diesel.DieselEngineScope.Being._
   import com.biosimilarity.evaluator.distribution.diesel.DieselEngineCtor._
   import com.biosimilarity.lift.model.store._
+  import com.biosimilarity.evaluator.distribution.diesel.EvalNodeMapper
   import com.mongodb.casbah.Imports._
 
-  def erqlInstance() : CnxnCtxtLabel[String,String,String] = {
-    DSLCommLinkCtor.ExchangeLabels.evalRequestLabel()( Left[String,String]( java.util.UUID.randomUUID().toString ) ).getOrElse( throw new \
-Exception( "error making evalRequestLabel" ) )
-  }
-  def kQuery(
-       channel : StdEvalChannel,
-       collectionName : String,
-       ptn : CnxnCtxtLabel[String,String,String]
-      ) : Option[DBObject] = {
-    for(
-      pm <- channel.cache.persistenceManifest;
-      qry <- pm match {
-	case mpm : MongoDBManifest => {
-          mpm.kqueryDBObject( collectionName, ptn )
-	}
-        case _ => {
-          None
-	}
-      }
-    ) yield {
-      qry
+  def erqlInstance(): CnxnCtxtLabel[String, String, String] =
+    DSLCommLinkCtor.ExchangeLabels
+      .evalRequestLabel()(Left[String, String](java.util.UUID.randomUUID().toString))
+      .getOrElse(throw new Exception("error making evalRequestLabel"))
+
+  def stdEvalChannel: Option[StdEvalChannel] = EvalNodeMapper.headOption.map(_._2)
+
+  def evalRequestLabel(s: String) = DSLCommLinkCtor.ExchangeLabels.evalRequestLabel()(Right(s))
+
+  def mongoDBManifest: Option[MongoDBManifest] = stdEvalChannel.flatMap { (channel: StdEvalChannel) =>
+    channel.cache.persistenceManifest match {
+      case Some(mpm: MongoDBManifest) => Some(mpm)
+      case _                          => None
     }
   }
+
+  def kQuery(channel: StdEvalChannel, collectionName: String, ptn: CnxnCtxtLabel[String, String, String]): Option[DBObject] =
+    mongoDBManifest.flatMap { (manifest: MongoDBManifest) =>
+      manifest.kqueryDBObject(collectionName, ptn)
+    }
 }
