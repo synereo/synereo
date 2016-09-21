f8cbc919e574a8360b3284a7975d735adf7202db
Henry Till <henrytill@gmail.com>
2016-09-21 19:48:13 -0400

    gloseval: fix json4s import in AgentCRUDHandler

--- gloseval/src/main/scala/com/biosimilarity/evaluator/spray/AgentCRUDHandler.scala
+++ gloseval/src/main/scala/com/biosimilarity/evaluator/spray/AgentCRUDHandler.scala
@@ -21,7 +21,7 @@ import spray.http._
 
 import org.json4s._
 import org.json4s.native.JsonMethods._
-import org.json4s.JsonDSL._
+import org.json4s.JsonDSL.{symbol2jvalue => _, _}
 
 
 import java.net.URI
